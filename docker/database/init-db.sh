ff47da7237d5232d7c5ba4007672809cc71f07c8
Abi-Laver <abi.laver@cgi.com>
2021-10-07 11:06:03 +0100

    add Dockerfile

---
+++ docker/database/init-db.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$HMC_HEARING_MANAGEMENT_DB_USERNAME" ] || [ -z "$HMC_HEARING_MANAGEMENT_DB_PASSWORD" ]; then
+  echo "ERROR: Missing environment variable. Set value for both 'HMC_HEARING_MANAGEMENT_DB_USERNAME' and 'HMC_HEARING_MANAGEMENT_DB_PASSWORD'."
+  exit 1
+fi
+
+# Create role and database
+psql -v ON_ERROR_STOP=1 --username postgres --set USERNAME=$HMC_HEARING_MANAGEMENT_DB_USERNAME --set PASSWORD=$HMC_HEARING_MANAGEMENT_DB_PASSWORD <<-EOSQL
+  CREATE USER :USERNAME WITH PASSWORD ':PASSWORD';
+  CREATE DATABASE hmc_hearing_service
+    WITH OWNER = :USERNAME
+    ENCODING = 'UTF-8'
+    CONNECTION LIMIT = -1;
+  ALTER SCHEMA public OWNER TO :USERNAME;
+EOSQL
