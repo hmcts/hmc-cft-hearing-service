15a8b72809742d3cec10fc0e1383b43b56a90c7c
markdathorne <mark.dathorne@cgi.com>
2021-10-08 11:05:30 +0100

    HMAN-24 revert dummy change just to trigger build pipeline

--- src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/EmptySmokeTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/EmptySmokeTest.java
@@ -4,14 +4,12 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 class EmptySmokeTest {
 
     @Tag("smoke")
     @Test
     void shouldRetrieveWhenExists() {
-        assertTrue(true);
+        Assert.assertTrue(true);
     }
 
 }
