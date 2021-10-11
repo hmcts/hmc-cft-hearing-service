6db274f9020b7f7baea710c98a4ad1ea99075445
markdathorne <mark.dathorne@cgi.com>
2021-10-11 12:02:49 +0100

    HMAN-24 trial pact config

--- src/test/java/uk/gov/hmcts/reform/rsecheck/DemoUnitTest.java
+++ src/test/java/uk/gov/hmcts/reform/rsecheck/DemoUnitTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.rsecheck;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DemoUnitTest {
@@ -10,6 +9,5 @@ class DemoUnitTest {
     @Test
     void exampleOfTest() {
         assertTrue(System.currentTimeMillis() > 0, "Example of Unit Test");
-        assertFalse(false, "basic false test");
     }
 }
