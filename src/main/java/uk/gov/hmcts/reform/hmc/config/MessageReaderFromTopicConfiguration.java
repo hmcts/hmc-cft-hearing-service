771a8bf8a3840f8309b4fe2ded7446eaabf30462
padmapriyanalam <padmapriya.nalam@gmail.com>
2021-10-21 14:17:29 +0100

    HMAN-29 -Check style issues

--- src/main/java/uk/gov/hmcts/reform/hmc/config/MessageReaderFromTopicConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/config/MessageReaderFromTopicConfiguration.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.hmc.config;
 
-
 import com.azure.messaging.servicebus.ServiceBusClientBuilder;
 import com.azure.messaging.servicebus.ServiceBusErrorContext;
 import com.azure.messaging.servicebus.ServiceBusException;
@@ -23,12 +22,9 @@ import java.util.concurrent.TimeUnit;
 public class MessageReaderFromTopicConfiguration {
 
     private final ApplicationParams applicationParams;
-    private final MessageProcessor messageProcessor;
 
-    public MessageReaderFromTopicConfiguration(ApplicationParams applicationParams,
-                                               MessageProcessor messageProcessor) {
+    public MessageReaderFromTopicConfiguration(ApplicationParams applicationParams) {
         this.applicationParams = applicationParams;
-        this.messageProcessor = messageProcessor;
     }
 
     @Async
@@ -61,32 +57,26 @@ public class MessageReaderFromTopicConfiguration {
 
     private static void processMessage(ServiceBusReceivedMessageContext context) {
         ServiceBusReceivedMessage message = context.getMessage();
-        log.info("Received message with id '{}'", message.getMessageId());
+        log.info("Received message with id '{}', message: {}", message.getMessageId(), message.getBody());
     }
 
     private static void processError(ServiceBusErrorContext context, CountDownLatch countdownLatch) {
-        log.error("Error when receiving messages from namespace: '%s'. Entity: '%s'%n",
-                  context.getFullyQualifiedNamespace(), context.getEntityPath()
-        );
-
+        log.error("Error when receiving messages from namespace: {}. Entity: {}",
+                  context.getFullyQualifiedNamespace(), context.getEntityPath());
         if (!(context.getException() instanceof ServiceBusException)) {
-            log.error("Non-ServiceBusException occurred: %s%n", context.getException());
+            log.error("Non-ServiceBusException occurred: {}", context.getException());
             return;
         }
-
         ServiceBusException exception = (ServiceBusException) context.getException();
         ServiceBusFailureReason reason = exception.getReason();
-
         if (reason == ServiceBusFailureReason.MESSAGING_ENTITY_DISABLED
             || reason == ServiceBusFailureReason.MESSAGING_ENTITY_NOT_FOUND
             || reason == ServiceBusFailureReason.UNAUTHORIZED) {
-            log.error("An unrecoverable error occurred. Stopping processing with reason %s: %s%n",
-                      reason, exception.getMessage()
-            );
-
+            log.error("An unrecoverable error occurred. Stopping processing with reason {}, message: {}",
+                      reason, exception.getMessage());
             countdownLatch.countDown();
         } else if (reason == ServiceBusFailureReason.MESSAGE_LOCK_LOST) {
-            log.error("Message lock lost for message: %s%n", context.getException());
+            log.error("Message lock lost for message: {}", context.getException());
         } else if (reason == ServiceBusFailureReason.SERVICE_BUSY) {
             try {
                 // Choosing an arbitrary amount of time to wait until trying again.
@@ -95,7 +85,7 @@ public class MessageReaderFromTopicConfiguration {
                 log.error("Unable to sleep for period of time");
             }
         } else {
-            log.error("Error source %s, reason %s, message: %s%n", context.getErrorSource(),
+            log.error("Error source {}, reason {}, message: {}", context.getErrorSource(),
                       reason, context.getException()
             );
         }
