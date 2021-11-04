79a7a24ec6074234ad3c9414c8ad0a916b869b6e
markdathorne <mark.dathorne@cgi.com>
2021-11-04 12:36:27 +0000

    HMAN-25 Trigger another build

--- src/main/java/uk/gov/hmcts/reform/hmc/Application.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/Application.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SuppressWarnings("HideUtilityClassConstructor")
 // Spring needs a constructor, it's not a utility class
 public class Application {
-
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
