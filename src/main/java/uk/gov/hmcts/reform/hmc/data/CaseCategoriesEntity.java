5192602e51e04cef563e09e730304033a1d24954
rebeccabaker <rebecca.baker@hmcts.net>
2021-11-19 11:12:22 +0000

    HMAN-27 manually point to sequences

--- src/main/java/uk/gov/hmcts/reform/hmc/data/CaseCategoriesEntity.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/data/CaseCategoriesEntity.java
@@ -9,14 +9,17 @@ import javax.persistence.*;
 @Table(name = "case_categories")
 @Entity
 @Data
+@SecondaryTable(name="CASE_HEARING_REQUEST",
+    pkJoinColumns={
+        @PrimaryKeyJoinColumn(name="CASE_HEARING_ID")})
 public class CaseCategoriesEntity {
 
     @Id
-    @SequenceGenerator(name="case_hearing_id_seq",
-        sequenceName="case_hearing_id_seq",
+    @SequenceGenerator(name="case_categories_id_seq",
+        sequenceName="case_categories_id_seq",
         allocationSize=1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE,
-        generator="case_hearing_id_seq")
+        generator="case_categories_id_seq")
     @Column(name = "id")
     private Long id;
 
