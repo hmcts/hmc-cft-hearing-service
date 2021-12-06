72e88eaa960834c15cea8be9b7885ab9e08fafad
rebeccabaker <rebecca.baker@hmcts.net>
2021-12-06 15:41:09 +0000

    HMAN-33 removed unused import

--- src/integrationTest/java/uk/gov/hmcts/reform/hmc/controllers/HearingManagementControllerIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/hmc/controllers/HearingManagementControllerIT.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.hmc.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
