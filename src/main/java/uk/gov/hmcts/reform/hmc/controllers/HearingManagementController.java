e68b0d454005af2f2c10badc10258c27fe781ad2
padmapriyanalam <padmapriya.nalam@gmail.com>
2021-12-07 17:39:56 +0000

    HMAN-32 - Merge conflicts

--- src/main/java/uk/gov/hmcts/reform/hmc/controllers/HearingManagementController.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/controllers/HearingManagementController.java
@@ -5,8 +5,6 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.hibernate.validator.constraints.LuhnCheck;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
