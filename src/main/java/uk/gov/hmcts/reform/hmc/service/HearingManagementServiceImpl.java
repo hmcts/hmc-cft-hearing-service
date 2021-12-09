6925564b451ed819c0a4ed7dc7b029836a444861
Abi-Laver <abi.laver@cgi.com>
2021-12-09 10:13:08 +0000

    HMAN-37 fix checkstyle after merge

--- src/main/java/uk/gov/hmcts/reform/hmc/service/HearingManagementServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/service/HearingManagementServiceImpl.java
@@ -97,6 +97,19 @@ public class HearingManagementServiceImpl implements HearingManagementService {
         validateVersionNumber(hearingId, hearingRequest.getRequestDetails().getVersionNumber());
     }
 
+    /**
+     * validate Get Hearing Request by caseRefId or caseRefId/caseStatus.
+     * @param caseRef case Ref
+     * @param status status
+     * @return HearingRequest HearingRequest
+     */
+    @Override
+    public HearingRequest validateGetHearingsRequest(String caseRef, String status) {
+        log.info("caseRef:{} ; status:{}", caseRef, status);
+        // TODO: select hearing request from given caseRefId and status (if any)
+        return new HearingRequest();
+    }
+
     private HearingResponse insertHearingRequest(HearingRequest hearingRequest) {
         HearingEntity savedEntity = saveHearingDetails(hearingRequest);
         return getSaveHearingResponseDetails(savedEntity);
@@ -123,19 +136,6 @@ public class HearingManagementServiceImpl implements HearingManagementService {
         if (hearingRequest.getPartyDetails() != null) {
             validatePartyDetails(hearingRequest.getPartyDetails());
         }
-    }
-
-    /**
-     * validate Get Hearing Request by caseRefId or caseRefId/caseStatus.
-     * @param caseRef case Ref
-     * @param status status
-     * @return HearingRequest HearingRequest
-     */
-    @Override
-    public HearingRequest validateGetHearingsRequest(String caseRef, String status) {
-        log.info("caseRef:{} ; status:{}", caseRef, status);
-        // TODO: select hearing request from given caseRefId and status (if any)
-        return new HearingRequest();
     }
 
     private void validateHearingRequest(UpdateHearingRequest hearingRequest) {
