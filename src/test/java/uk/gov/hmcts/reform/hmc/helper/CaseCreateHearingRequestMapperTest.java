71856b9cf284ffeffc3781ef1480c260002dfe0b
Abi-Laver <abi.laver@cgi.com>
2022-01-05 17:57:49 +0000

    HMAN-31 code smell fix

--- src/test/java/uk/gov/hmcts/reform/hmc/helper/CaseCreateHearingRequestMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/helper/CaseCreateHearingRequestMapperTest.java
@@ -9,49 +9,47 @@ import uk.gov.hmcts.reform.hmc.utils.TestingUtil;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 class CaseCreateHearingRequestMapperTest {
 
     @Test
     void modelToEntity() {
+        CaseHearingRequestEntity expectedEntity = new CaseHearingRequestEntity();
+        expectedEntity.setAutoListFlag(true);
+        expectedEntity.setHearingType("Some hearing type");
+        expectedEntity.setRequiredDurationInMinutes(0);
+        expectedEntity.setHearingPriorityType("Priority type");
+        expectedEntity.setPrivateHearingRequiredFlag(true);
+        expectedEntity.setHmctsServiceID("ABA1");
+        expectedEntity.setCaseReference("1111222233334444");
+        expectedEntity.setHearingRequestReceivedDateTime(LocalDateTime.parse("2021-08-10T12:20:00"));
+        expectedEntity.setCaseUrlContextPath("https://www.google.com");
+        expectedEntity.setHmctsInternalCaseName("Internal case name");
+        expectedEntity.setPublicCaseName("Public case name");
+        expectedEntity.setAdditionalSecurityRequiredFlag(false);
+        expectedEntity.setOwningLocationId("CMLC123");
+        expectedEntity.setCaseRestrictedFlag(false);
+        expectedEntity.setVersionNumber(1);
+        expectedEntity.setIsLinkedFlag(false);
+        expectedEntity.setHearingWindowStartDateRange(LocalDate.parse("2017-03-01"));
+        expectedEntity.setHearingWindowEndDateRange(LocalDate.parse("2017-03-01"));
+        expectedEntity.setCaseSlaStartDate(LocalDate.parse("2017-03-01"));
         CreateHearingRequest createHearingRequest = new CreateHearingRequest();
         createHearingRequest.setRequestDetails(TestingUtil.requestDetails());
+        LocalDateTime time = LocalDateTime.now();
+        createHearingRequest.getRequestDetails().setRequestTimeStamp(time);
         createHearingRequest.setHearingDetails(TestingUtil.hearingDetails());
         createHearingRequest.setCaseDetails(TestingUtil.caseDetails());
         HearingEntity hearingEntity = new HearingEntity();
         CaseCategoriesMapper caseCategoriesMapper = new CaseCategoriesMapper();
         CaseHearingRequestMapper caseHearingRequestMapper = new CaseHearingRequestMapper(caseCategoriesMapper);
-        CaseHearingRequestEntity entity = caseHearingRequestMapper.modelToEntity(createHearingRequest, hearingEntity);
-        assertAll(
-            () -> assertEquals(Boolean.TRUE, entity.getAutoListFlag()),
-            () -> assertEquals("Some hearing type", entity.getHearingType()),
-            () -> assertEquals(0, entity.getRequiredDurationInMinutes()),
-            () -> assertEquals("Priority type", entity.getHearingPriorityType()),
-            () -> assertNull(entity.getNumberOfPhysicalAttendees()),
-            () -> assertNull(entity.getHearingInWelshFlag()),
-            () -> assertEquals(Boolean.TRUE, entity.getPrivateHearingRequiredFlag()),
-            () -> assertNull(entity.getLeadJudgeContractType()),
-            () -> assertNull(entity.getFirstDateTimeOfHearingMustBe()),
-            () -> assertEquals("ABA1", entity.getHmctsServiceID()),
-            () -> assertEquals("1111222233334444", entity.getCaseReference()),
-            () -> assertEquals(LocalDateTime.parse("2021-08-10T12:20:00"), entity.getHearingRequestReceivedDateTime()),
-            () -> assertNull(entity.getExternalCaseReference()),
-            () -> assertEquals("https://www.google.com", entity.getCaseUrlContextPath()),
-            () -> assertEquals("Internal case name", entity.getHmctsInternalCaseName()),
-            () -> assertEquals("Public case name", entity.getPublicCaseName()),
-            () -> assertEquals(Boolean.FALSE, entity.getAdditionalSecurityRequiredFlag()),
-            () -> assertEquals("CMLC123", entity.getOwningLocationId()),
-            () -> assertEquals(Boolean.FALSE, entity.getCaseRestrictedFlag()),
-            () -> assertEquals(1, entity.getVersionNumber()),
-            () -> assertNull(entity.getInterpreterBookingRequiredFlag()),
-            () -> assertEquals(Boolean.FALSE, entity.getIsLinkedFlag()),
-            () -> assertNull(entity.getListingComments()),
-            () -> assertNull(entity.getRequester()),
-            () -> assertEquals(LocalDate.parse("2017-03-01"), entity.getHearingWindowStartDateRange()),
-            () -> assertEquals(LocalDate.parse("2017-03-01"), entity.getHearingWindowEndDateRange())
+        CaseHearingRequestEntity actualEntity = caseHearingRequestMapper.modelToEntity(
+            createHearingRequest,
+            hearingEntity
         );
+        expectedEntity.setHearing(hearingEntity);
+        expectedEntity.setRequestTimeStamp(time);
+        assertEquals(expectedEntity, actualEntity);
     }
 }
