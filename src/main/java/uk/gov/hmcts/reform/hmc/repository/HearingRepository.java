162c82ecbe78f7a650df54e902c7db83ce289e0c
padmapriyanalam <padmapriya.nalam@gmail.com>
2022-01-06 13:52:13 +0000

    HMAN-39 - Implementation

---
+++ src/main/java/uk/gov/hmcts/reform/hmc/repository/HearingRepository.java
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.reform.hmc.data;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+@Repository
+public interface HearingRepository extends CrudRepository<HearingEntity, Long> {
+
+    @Query("SELECT status from HearingEntity where id = :hearingId")
+    String getStatus(Long hearingId);
+}
