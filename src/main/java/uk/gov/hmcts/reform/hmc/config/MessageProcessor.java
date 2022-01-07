46611959236c2f92e6f8093fc8f067f7dea886b3
rebeccabaker <rebecca.baker@hmcts.net>
2022-01-07 14:02:15 +0000

    HMAN-54 add dummy processor class

--- src/main/java/uk/gov/hmcts/reform/hmc/config/MessageProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/config/MessageProcessor.java
@@ -1,16 +1,10 @@
 package uk.gov.hmcts.reform.hmc.config;
 
-import com.azure.core.util.BinaryData;
 import com.azure.messaging.servicebus.ServiceBusReceivedMessage;
 import com.azure.messaging.servicebus.ServiceBusReceiverClient;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
-
 
 @Slf4j
 @Component
