d1302f6a55417861db6b85ac01d2ac2751e15f85
rebeccabaker <rebecca.baker@hmcts.net>
2022-01-07 11:30:46 +0000

    HMAN-54 uncomment class

--- src/main/java/uk/gov/hmcts/reform/hmc/config/MessageReaderFromQueueConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/config/MessageReaderFromQueueConfiguration.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.hmc.config;
 
 import com.azure.core.amqp.AmqpRetryMode;
 import com.azure.core.amqp.AmqpRetryOptions;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
 import com.azure.messaging.servicebus.ServiceBusReceiverClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -26,9 +27,9 @@ public class MessageReaderFromQueueConfiguration {
     @EventListener(ApplicationStartedEvent.class)
     @SuppressWarnings("squid:S2189")
     public void readMessageFromTopic() {
-        //log.info("Creating service bus receiver client");
+        log.info("Creating service bus receiver client");
 
-        /**ServiceBusReceiverClient client = new ServiceBusClientBuilder()
+        ServiceBusReceiverClient client = new ServiceBusClientBuilder()
             .connectionString(applicationParams.getInternalConnectionString())
             .retryOptions(retryOptions())
             .receiver()
@@ -37,19 +38,17 @@ public class MessageReaderFromQueueConfiguration {
 
         while (true) {
             receiveMessages(client);
-        } **/
+        }
     }
 
     // handles received messages
     public void receiveMessages(ServiceBusReceiverClient client) {
-       /**
         client.receiveMessages(1)
             .forEach(
                 message -> {
                     log.info("message received with Id " + message.getMessageId()
                                  + " and message body " + message.getBody());
                 });
-        **/
     }
 
     private AmqpRetryOptions retryOptions() {
