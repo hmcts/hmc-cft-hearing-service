487826410c16b07d0f567ca2daf3ab1f1c190b7a
markdathorne <mark.dathorne@cgi.com>
2022-01-20 11:37:25 +0000

    HMAN-45 Correct HEARING_STATUS constant to valid value HEARING_REQUESTED

--- src/main/java/uk/gov/hmcts/reform/hmc/constants/Constants.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/constants/Constants.java
@@ -5,7 +5,7 @@ public final class Constants {
     private Constants() {
     }
 
-    public static final String HEARING_STATUS = "HEARING REQUESTED";
+    public static final String HEARING_STATUS = "HEARING_REQUESTED";
     public static final Integer VERSION_NUMBER = 1;
     public static final String EMAIL_TYPE = "email";
     public static final String PHONE_TYPE = "phone";
