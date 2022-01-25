3cc581f80882c00ff7361988c4507f68652a005a
markdathorne <mark.dathorne@cgi.com>
2022-01-25 09:00:06 +0000

    HMAN-40 Fix dependency checks by postponing current CVE suppressions for another month pending investigation

--- src/main/java/uk/gov/hmcts/reform/hmc/Application.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/Application.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, it's not a utility class
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, it is not a utility class
 public class Application {
 
     public static void main(final String[] args) {
