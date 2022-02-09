bee8227beb3302c82d5deb2144a5d57654e6c6d8
markdathorne <mark.dathorne@cgi.com>
2022-02-09 11:44:05 +0000

    HMAN-42 Remove out of scope changes.

--- src/main/java/uk/gov/hmcts/reform/hmc/model/HearingDaySchedule.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/model/HearingDaySchedule.java
@@ -1,18 +1,12 @@
 package uk.gov.hmcts.reform.hmc.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.hmc.exceptions.ValidationError;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -20,14 +14,10 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 public class HearingDaySchedule {
 
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @NotNull
     private LocalDateTime hearingStartDateTime;
 
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @NotNull
     private LocalDateTime hearingEndDateTime;
 
     @JsonProperty("listAssistSessionID")
@@ -35,21 +25,22 @@ public class HearingDaySchedule {
     @NotNull
     private String listAssistSessionId;
 
-    @NotEmpty(message = ValidationError.HEARING_VENUE_ID_NULL_EMPTY)
-    @Size(max = 60, message = ValidationError.HEARING_VENUE_ID_MAX_LENGTH)
+    @Size(max = 60)
+    @NotNull
     private String hearingVenueId;
 
-    @NotEmpty(message = ValidationError.HEARING_ROOM_ID_NULL_EMPTY)
-    @Size(max = 60, message = ValidationError.HEARING_ROOM_ID_MAX_LENGTH)
-    private String hearingRoomId;
+    @Size(max = 60)
+    @NotNull
+    private String  hearingRoomId;
 
-    @NotEmpty(message = ValidationError.HEARING_JUDGE_ID_NULL_EMPTY)
-    @Size(max = 60, message = ValidationError.HEARING_JUDGE_ID_MAX_LENGTH)
+    @Size(max = 60)
     private String hearingJudgeId;
 
     @Size(max = 60)
     private String panelMemberId;
 
+    @Valid
+    @NotNull
     private List<Attendee> attendees;
 
 }
