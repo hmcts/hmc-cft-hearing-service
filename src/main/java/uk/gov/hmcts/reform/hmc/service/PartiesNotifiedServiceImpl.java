cf9505ce3e777088d3a662b99edb85890104a360
markdathorne <mark.dathorne@cgi.com>
2022-02-18 14:41:01 +0000

    HMAN-66 Trigger build

--- src/main/java/uk/gov/hmcts/reform/hmc/service/PartiesNotifiedServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/service/PartiesNotifiedServiceImpl.java
@@ -50,7 +50,7 @@ public class PartiesNotifiedServiceImpl implements PartiesNotifiedService {
     }
 
     /**
-     * TODO: refactor this validation method to a parent service class.
+     * validate Hearing id
      * @param hearingId hearing id
      */
     private void validateHearingId(Long hearingId) {
@@ -66,7 +66,7 @@ public class PartiesNotifiedServiceImpl implements PartiesNotifiedService {
     }
 
     /**
-     * TODO: refactor this validation method to a parent service class.
+     * validate Hearing id format
      * @param hearingIdStr hearing id string
      */
     private void isValidFormat(String hearingIdStr) {
