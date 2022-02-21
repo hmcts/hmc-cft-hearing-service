fbd2b2e1090f793a9dcbe22f26690668394a6674
rebeccabaker <rebecca.baker@hmcts.net>
2022-02-21 16:42:42 +0000

    HMAN-51 fixed failing tests

--- src/test/java/uk/gov/hmcts/reform/hmc/service/PartiesNotifiedServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/service/PartiesNotifiedServiceTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -71,7 +70,6 @@ class PartiesNotifiedServiceTest {
 
 
         @Test
-        @Disabled
         void shouldFailWithInvalidHearingId() throws JsonProcessingException {
             JsonNode jsonNode = new ObjectMapper().readTree("{\"query\": {\"match\": \"blah blah\"}}");
             PartiesNotified partiesNotified = generatePartiesNotified(1, jsonNode);
@@ -98,7 +96,6 @@ class PartiesNotifiedServiceTest {
         }
 
         @Test
-        @Disabled
         void shouldFailWithNoSuchId() throws JsonProcessingException {
             JsonNode jsonNode = new ObjectMapper().readTree("{\"query\": {\"match\": \"blah blah\"}}");
             PartiesNotified partiesNotified = generatePartiesNotified(1, jsonNode);
