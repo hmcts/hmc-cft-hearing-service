0ba928fb3915d86e598a139830a4c70792ba87cb
markdathorne <mark.dathorne@cgi.com>
2022-02-23 12:11:45 +0000

    HMAN-67 Fix quality gate issues - duplicated code.

---
+++ src/main/java/uk/gov/hmcts/reform/hmc/service/HearingValidation.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.hmc.service;
+
+import com.microsoft.applicationinsights.core.dependencies.apachecommons.lang3.StringUtils;
+import uk.gov.hmcts.reform.hmc.exceptions.BadRequestException;
+import uk.gov.hmcts.reform.hmc.exceptions.HearingNotFoundException;
+import uk.gov.hmcts.reform.hmc.repository.HearingRepository;
+
+import static uk.gov.hmcts.reform.hmc.constants.Constants.HEARING_ID_MAX_LENGTH;
+import static uk.gov.hmcts.reform.hmc.exceptions.ValidationError.INVALID_HEARING_ID_DETAILS;
+
+public class HearingValidation {
+
+    protected final HearingRepository hearingRepository;
+
+    public HearingValidation(HearingRepository hearingRepository) {
+        this.hearingRepository = hearingRepository;
+    }
+
+    /**
+     * validate Hearing id.
+     * @param hearingId hearing id
+     */
+    protected void validateHearingId(Long hearingId) {
+        if (hearingId == null) {
+            throw new BadRequestException(INVALID_HEARING_ID_DETAILS);
+        } else {
+            String hearingIdStr = String.valueOf(hearingId);
+            isValidFormat(hearingIdStr);
+            if (!hearingRepository.existsById(hearingId)) {
+                throw new HearingNotFoundException(hearingId);
+            }
+        }
+    }
+
+    /**
+     * validate Hearing id format.
+     * @param hearingIdStr hearing id string
+     */
+    protected void isValidFormat(String hearingIdStr) {
+        if (hearingIdStr.length() != HEARING_ID_MAX_LENGTH || !StringUtils.isNumeric(hearingIdStr)
+                || hearingIdStr.charAt(0) != '2') {
+            throw new BadRequestException(INVALID_HEARING_ID_DETAILS);
+        }
+    }
+}
--- src/main/java/uk/gov/hmcts/reform/hmc/service/PartiesNotifiedServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/service/PartiesNotifiedServiceImpl.java
@@ -1,13 +1,10 @@
 package uk.gov.hmcts.reform.hmc.service;
 
-import com.microsoft.applicationinsights.core.dependencies.apachecommons.lang3.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.hmc.data.PartiesNotifiedEntity;
-import uk.gov.hmcts.reform.hmc.exceptions.BadRequestException;
-import uk.gov.hmcts.reform.hmc.exceptions.HearingNotFoundException;
 import uk.gov.hmcts.reform.hmc.model.PartiesNotifiedResponse;
 import uk.gov.hmcts.reform.hmc.model.PartiesNotifiedResponses;
 import uk.gov.hmcts.reform.hmc.repository.HearingRepository;
@@ -16,21 +13,17 @@ import uk.gov.hmcts.reform.hmc.repository.HearingResponseRepository;
 import java.util.ArrayList;
 import java.util.List;
 
-import static uk.gov.hmcts.reform.hmc.constants.Constants.HEARING_ID_MAX_LENGTH;
-import static uk.gov.hmcts.reform.hmc.exceptions.ValidationError.INVALID_HEARING_ID_DETAILS;
-
 @Service
 @Component
 @Slf4j
-public class PartiesNotifiedServiceImpl implements PartiesNotifiedService {
+public class PartiesNotifiedServiceImpl extends HearingValidation implements PartiesNotifiedService {
 
-    private final HearingRepository hearingRepository;
     private final HearingResponseRepository hearingResponseRepository;
 
     @Autowired
     public PartiesNotifiedServiceImpl(HearingRepository hearingRepository,
-                                        HearingResponseRepository hearingResponseRepository) {
-        this.hearingRepository = hearingRepository;
+            HearingResponseRepository hearingResponseRepository) {
+        super(hearingRepository);
         this.hearingResponseRepository = hearingResponseRepository;
     }
 
@@ -66,30 +59,4 @@ public class PartiesNotifiedServiceImpl implements PartiesNotifiedService {
         return responses;
     }
 
-    /**
-     * validate Hearing id.
-     * @param hearingId hearing id.
-     */
-    private void validateHearingId(Long hearingId) {
-        if (hearingId == null) {
-            throw new BadRequestException(INVALID_HEARING_ID_DETAILS);
-        } else {
-            String hearingIdStr = String.valueOf(hearingId);
-            isValidFormat(hearingIdStr);
-            if (!hearingRepository.existsById(hearingId)) {
-                throw new HearingNotFoundException(hearingId);
-            }
-        }
-    }
-
-    /**
-     * validate Hearing id format.
-     * @param hearingIdStr hearing id string
-     */
-    private void isValidFormat(String hearingIdStr) {
-        if (hearingIdStr.length() != HEARING_ID_MAX_LENGTH || !StringUtils.isNumeric(hearingIdStr)
-                || hearingIdStr.charAt(0) != '2') {
-            throw new BadRequestException(INVALID_HEARING_ID_DETAILS);
-        }
-    }
 }
