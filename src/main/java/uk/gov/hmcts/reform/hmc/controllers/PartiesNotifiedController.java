6f82dab3059431f3dcfdc48cf7094bcf7b3f2b86
rebeccabaker <rebecca.baker@hmcts.net>
2022-02-24 16:34:25 +0000

    HMAN-51 HMAN-52 HMAN-66 and HMAN-67 fixed checkstyle

--- src/main/java/uk/gov/hmcts/reform/hmc/controllers/PartiesNotifiedController.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/controllers/PartiesNotifiedController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.reform.hmc.model.partiesnotified.PartiesNotifiedResponses;
 import uk.gov.hmcts.reform.hmc.model.partiesnotified.PartiesNotified;
+import uk.gov.hmcts.reform.hmc.model.partiesnotified.PartiesNotifiedResponses;
 import uk.gov.hmcts.reform.hmc.service.PartiesNotifiedService;
 
 import javax.validation.Valid;
--- src/main/java/uk/gov/hmcts/reform/hmc/service/PartiesNotifiedService.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/service/PartiesNotifiedService.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.hmc.service;
 
-import uk.gov.hmcts.reform.hmc.model.partiesnotified.PartiesNotifiedResponses;
 import uk.gov.hmcts.reform.hmc.model.partiesnotified.PartiesNotified;
+import uk.gov.hmcts.reform.hmc.model.partiesnotified.PartiesNotifiedResponses;
 
 public interface PartiesNotifiedService {
 
