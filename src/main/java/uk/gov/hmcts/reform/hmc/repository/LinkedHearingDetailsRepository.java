13a9ad14ec4293e99599da8c22fd836d264ffbf0
rebeccabaker <rebecca.baker@hmcts.net>
2022-03-01 15:45:25 +0000

    HMAN-55 WIP updated some validation

---
+++ src/main/java/uk/gov/hmcts/reform/hmc/repository/LinkedHearingDetailsRepository.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.hmc.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.reform.hmc.data.CaseHearingRequestEntity;
+import uk.gov.hmcts.reform.hmc.data.HearingEntity;
+import uk.gov.hmcts.reform.hmc.data.LinkedHearingDetails;
+
+import java.util.List;
+
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+@Repository
+public interface LinkedHearingDetailsRepository extends CrudRepository<LinkedHearingDetails, Long> {
+
+    @Query("from LinkedHearingDetails lhd WHERE lhd.hearingId = :hearingId ")
+    List<LinkedHearingDetails> getLinkedHearingDetailsById(long hearingId);
+}
--- src/main/java/uk/gov/hmcts/reform/hmc/service/LinkedHearingGroupServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/service/LinkedHearingGroupServiceImpl.java
@@ -5,23 +5,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.hmc.data.HearingEntity;
+import uk.gov.hmcts.reform.hmc.data.LinkedHearingDetails;
+import uk.gov.hmcts.reform.hmc.domain.model.enums.PutHearingStatus;
 import uk.gov.hmcts.reform.hmc.exceptions.BadRequestException;
 import uk.gov.hmcts.reform.hmc.model.HearingResponse;
+import uk.gov.hmcts.reform.hmc.model.LinkType;
 import uk.gov.hmcts.reform.hmc.model.linkedHearingGroup.HearingLinkGroupRequest;
 import uk.gov.hmcts.reform.hmc.repository.HearingRepository;
+import uk.gov.hmcts.reform.hmc.repository.LinkedHearingDetailsRepository;
 import uk.gov.hmcts.reform.hmc.validator.HearingIdValidator;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
+
+import static uk.gov.hmcts.reform.hmc.exceptions.ValidationError.INVALID_PUT_HEARING_STATUS;
 
 @Service
 @Component
 @Slf4j
 public class LinkedHearingGroupServiceImpl extends HearingIdValidator implements LinkedHearingGroupService {
 
+    private final LinkedHearingDetailsRepository linkedHearingDetailsRepository;
 
     @Autowired
-    public LinkedHearingGroupServiceImpl(HearingRepository hearingRepository) {
+    public LinkedHearingGroupServiceImpl(HearingRepository hearingRepository,
+                                         LinkedHearingDetailsRepository linkedHearingDetailsRepository) {
         super(hearingRepository);
+        this.linkedHearingDetailsRepository = linkedHearingDetailsRepository;
 
     }
 
@@ -38,10 +49,24 @@ public class LinkedHearingGroupServiceImpl extends HearingIdValidator implements
                 }
             }
             //hearing id  in linkedHearingDetails check if its in a group
-            // if so throw error
-            //check hearing status
-            //check date of hearing
-            //check date of hearing order
+            List<LinkedHearingDetails> lhd = linkedHearingDetailsRepository.getLinkedHearingDetailsById(Long.valueOf(
+                linkHearingDetails.getHearingId()));
+
+            if (!lhd.isEmpty()) {
+                // if so throw error
+            }
+            if (!PutHearingStatus.isValid(lhd.get(0).getLinkedGroup().getStatus())) {
+                //check hearing status
+                //  throw new BadRequestException();
+            }
+            if (LocalDate.now().isBefore(lhd.get(0).getLinkedGroup().getRequestDateTime().toLocalDate())) {
+                //check date of hearing
+                //  throw new BadRequestException();
+            }
+            if (LinkType.ORDERED.equals(lhd.get(0).getLinkedGroup().getLinkType())) {
+                //check link order is unqiue
+                Long linkedOrder = lhd.get(0).getLinkedOrder();
+            }
         });
     }
 }
