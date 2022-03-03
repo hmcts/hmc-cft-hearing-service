4c9be507212b6cb57d2d753b2a9b0afca5bf38d4
markdathorne <mark.dathorne@cgi.com>
2022-03-03 09:46:06 +0000

    HMAN-56 Extend unit tests for linkedHearingValidation

--- src/test/java/uk/gov/hmcts/reform/hmc/validation/LinkedHearingValidationTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/validation/LinkedHearingValidationTest.java
@@ -8,15 +8,17 @@ import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.reform.hmc.exceptions.BadRequestException;
+import uk.gov.hmcts.reform.hmc.exceptions.LinkedGroupNotFoundException;
 import uk.gov.hmcts.reform.hmc.repository.HearingRepository;
 import uk.gov.hmcts.reform.hmc.repository.LinkedGroupDetailsRepository;
 import uk.gov.hmcts.reform.hmc.repository.LinkedHearingDetailsRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.hmc.exceptions.ValidationError.LINKED_GROUP_ID_EMPTY;
 
-public class LinkedHearingValidationTest {
+class LinkedHearingValidationTest {
 
     private static final Logger logger = LoggerFactory.getLogger(LinkedHearingValidationTest.class);
 
@@ -36,16 +38,35 @@ public class LinkedHearingValidationTest {
     public void setUp() {
         MockitoAnnotations.openMocks(this);
         linkedHearingValidation =
-                new LinkedHearingValidation(hearingRepository,
-                                            linkedGroupDetailsRepository,
-                                            linkedHearingDetailsRepository);
+                new LinkedHearingValidation(hearingRepository, linkedGroupDetailsRepository,
+                        linkedHearingDetailsRepository);
     }
+
     @Test
     void shouldFailAsRequestIdIsNull() {
         Long requestId = null;
         Exception exception = assertThrows(BadRequestException.class, () -> linkedHearingValidation
                 .validateRequestId(requestId, null));
         assertEquals(LINKED_GROUP_ID_EMPTY, exception.getMessage());
+    }
+
+    @Test
+    void shouldFailAsRequestIdDoesNotExist() {
+        Long requestId = 9176L;
+        String errorMessage = "This is a test error message";
+        when(linkedGroupDetailsRepository.existsById(requestId)).thenReturn(false);
+
+        Exception exception = assertThrows(LinkedGroupNotFoundException.class, () -> linkedHearingValidation
+                .validateRequestId(requestId, errorMessage));
+        assertEquals(errorMessage, exception.getMessage());
+    }
+
+    @Test
+    void shouldSucceedAsRequestIdDoesExist() {
+        Long requestId = 9176L;
+        String errorMessage = "This is a test error message";
+        when(linkedGroupDetailsRepository.existsById(requestId)).thenReturn(true);
+        linkedHearingValidation.validateRequestId(requestId, errorMessage);
     }
 
 }
