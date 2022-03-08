206802c9a0486a752cc7e430ff2e57b34dc393ea
rebeccabaker <rebecca.baker@hmcts.net>
2022-03-08 15:32:44 +0000

    HMAN-101 refactor linked Hearing tables and relevant entities

--- src/main/java/uk/gov/hmcts/reform/hmc/data/LinkedGroupDetailsAudit.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/data/LinkedGroupDetailsAudit.java
@@ -35,28 +35,28 @@ public class LinkedGroupDetailsAudit {
     @Column(name = "linked_group_version")
     private Long linkedGroupVersion;
 
-    @Column(name = "request_id", nullable = false)
-    private String requestId;
-
-    @Column(name = "request_name")
-    private String requestName;
-
-    @Column(name = "request_date_time", nullable = false)
-    private LocalDateTime requestDateTime;
+    @Column(name = "linked_comments")
+    private String linkedComments;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "link_type", nullable = false)
     @Type(type = "uk.gov.hmcts.reform.hmc.model.LinkType")
     private String linkType;
 
+    @Column(name = "request_id", nullable = false)
+    private String requestId;
+
+    @Column(name = "request_name")
+    private String requestName;
+
     @Column(name = "reason_for_link", nullable = false)
     private String reasonForLink;
 
+    @Column(name = "request_date_time", nullable = false)
+    private LocalDateTime requestDateTime;
+
     @Column(name = "status", nullable = false)
     private String status;
-
-    @Column(name = "linked_comments")
-    private String linkedComments;
 
 }
 
