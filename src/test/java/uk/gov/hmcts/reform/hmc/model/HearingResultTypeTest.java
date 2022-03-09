9e64fa8d3a1b29bfed6e0625578e33c8f5cdc420
simchandramouly-hmcts <sim.chandramouly@hmcts.net>
2022-03-09 12:21:41 +0000

    Fixed checkstyle error

--- src/test/java/uk/gov/hmcts/reform/hmc/model/HearingResultTypeTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/model/HearingResultTypeTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
-import static org.junit.Assert.assertEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 
 class HearingResultTypeTest {
 
