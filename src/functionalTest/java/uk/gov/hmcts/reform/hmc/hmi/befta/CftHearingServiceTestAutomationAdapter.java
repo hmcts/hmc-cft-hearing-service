c413135e2dbb685e273f7b1222eb709bb7a50645
ashleynoronha <ashleydeannoronha@gmail.com>
2022-03-09 14:31:28 +0000

    checkstyle fix

--- src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/CftHearingServiceTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/CftHearingServiceTestAutomationAdapter.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.hmc.hmi.befta;
 import uk.gov.hmcts.befta.BeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
-import uk.gov.hmcts.befta.util.BeftaUtils;
 
 public class CftHearingServiceTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
