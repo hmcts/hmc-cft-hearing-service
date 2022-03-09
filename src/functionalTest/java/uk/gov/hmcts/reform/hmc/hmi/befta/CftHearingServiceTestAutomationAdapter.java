120e1c07bc62c0b0110602c3db18c38cb17c0897
ashleynoronha <ashleydeannoronha@gmail.com>
2022-03-09 17:26:09 +0000

    add debuging to build gradle

--- src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/CftHearingServiceTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/CftHearingServiceTestAutomationAdapter.java
@@ -3,14 +3,21 @@ package uk.gov.hmcts.reform.hmc.hmi.befta;
 import uk.gov.hmcts.befta.BeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
+import uk.gov.hmcts.befta.util.BeftaUtils;
 
 public class CftHearingServiceTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
     @Override
     protected BeftaTestDataLoader buildTestDataLoader() {
-        return new DataLoaderToDefinitionStore(
-            this,
-            DataLoaderToDefinitionStore.VALID_CCD_TEST_DEFINITIONS_PATH
-        );
+        return new DataLoaderToDefinitionStore(this,
+                                               DataLoaderToDefinitionStore.VALID_CCD_TEST_DEFINITIONS_PATH) {
+
+            @Override
+            protected void createRoleAssignment(String resource, String filename) {
+                // Do not create role assignments.
+                BeftaUtils.defaultLog("Will NOT create role assignments!");
+            }
+
+        };
     }
 }
