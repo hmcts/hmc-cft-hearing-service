5740ee55c3666958e57f8e8ec87ba941ebe8e3a9
rebeccabaker <rebecca.baker@hmcts.net>
2022-03-23 13:46:50 +0000

    HMAN-79 Fixed integration test

--- src/integrationTest/resources/sql/get-caseHearings_request.sql
+++ src/integrationTest/resources/sql/get-caseHearings_request.sql
@@ -1,4 +1,4 @@
-insert into hearing ( hearing_id, status) values ('2000000000', 'UPDATE_REQUESTED');
+insert into hearing ( hearing_id, status) values ('2000000000', 'HEARING_REQUESTED');
 insert into hearing ( hearing_id, status) values ('2000000010', 'HEARING_UPDATED');
 insert into hearing ( hearing_id, status) values ('2000000009', 'HEARING_REQUESTED');
 
@@ -13,28 +13,28 @@ insert into case_hearing_request (
 values ( 't'	,'hearingType3',	63,	'Priority type3',	6,'t','t','AB123',null,'ABA1'	,9372710950276233,	'2020-08-10 11:20:00','EXT/REF123',	'https://www.google.com',	'Internal case name','Public case name',	't'	,'CMLC333',	't',	'2021-10-10 00:00:00',	1,	2000000009	,'f'	,	'Some listing comments3',	'Some judge3',	'2021-11-01 00:00:00',	'2021-11-12 00:00:00',	'2021-08-10 11:20:00');
 
 insert into hearing_response(hearing_response_id, hearing_id, received_date_time, listing_status, listing_case_status, response_version, request_version)
-values (51, '2000000000', '2020-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '2', '1');
+values (1, '2000000000', '2020-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '2', '1');
 insert into hearing_response(hearing_response_id, hearing_id, received_date_time, listing_status, listing_case_status, response_version, request_version, parties_notified_datetime)
-values (52, '2000000010', '2021-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '2', '1', '2020-08-10 11:20:00');
+values (2, '2000000010', '2021-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '2', '1', '2020-08-10 11:20:00');
 insert into hearing_response(hearing_response_id, hearing_id, received_date_time, listing_status, listing_case_status, response_version, request_version)
-values (53, '2000000009', '2021-08-10 11:20:00', 'listingStatus3-1', 'caselistingStatus3-1', '2', '1');
+values (3, '2000000009', '2021-08-10 11:20:00', 'listingStatus3-1', 'caselistingStatus3-1', '2', '1');
 
 insert into hearing_day_details(hearing_day_id, hearing_response_id, start_date_time, end_date_time, list_assist_session_id, venue_id, room_id)
-values (61, 51, '2021-01-10 11:20:00', '2021-08-10 11:20:00', 'session1-1', 'venue1-1', 'room1-1');
+values (1, 1, '2021-01-10 11:20:00', '2021-08-10 11:20:00', 'session1-1', 'venue1-1', 'room1-1');
 insert into hearing_day_details(hearing_day_id, hearing_response_id, start_date_time, end_date_time, list_assist_session_id, venue_id, room_id)
-values (62, 51, '2020-01-10 11:20:00', '2020-08-10 11:20:00', 'session1-2', 'venue1-2', 'room1-2');
+values (2, 1, '2020-01-10 11:20:00', '2020-08-10 11:20:00', 'session1-2', 'venue1-2', 'room1-2');
 insert into hearing_day_details(hearing_day_id, hearing_response_id, start_date_time, end_date_time, list_assist_session_id, venue_id, room_id)
-values (63, 52, '2020-01-10 11:20:00', '2020-08-10 11:20:00', 'session1-2', 'venue2-1', 'room2-1');
+values (3, 2, '2020-01-10 11:20:00', '2020-08-10 11:20:00', 'session1-2', 'venue2-1', 'room2-1');
 insert into hearing_day_details(hearing_day_id, hearing_response_id, start_date_time, end_date_time, list_assist_session_id, venue_id, room_id)
-values (64, 53, '2020-01-10 11:20:00', '2020-08-10 11:20:00', 'session1-2', 'venue3-1', 'room3-1');
+values (4, 3, '2020-01-10 11:20:00', '2020-08-10 11:20:00', 'session1-2', 'venue3-1', 'room3-1');
 
-insert into hearing_day_panel(id, hearing_day_id, panel_user_id,is_presiding) values (71, 61, 'panel1-1','true');
-insert into hearing_day_panel(id, hearing_day_id, panel_user_id,is_presiding) values (72, 61, 'panel1-2','false');
-insert into hearing_day_panel(id, hearing_day_id, panel_user_id,is_presiding) values (73, 62, 'panel1-2',null);
-insert into hearing_day_panel(id, hearing_day_id, panel_user_id,is_presiding) values (74, 63, 'panel2-1','true');
-insert into hearing_day_panel(id, hearing_day_id, panel_user_id,is_presiding) values (75, 64, 'panel3-1','false');
+insert into hearing_day_panel(id, hearing_day_id, panel_user_id,is_presiding) values (1, 1, 'panel1-1','true');
+insert into hearing_day_panel(id, hearing_day_id, panel_user_id,is_presiding) values (2, 1, 'panel1-2','false');
+insert into hearing_day_panel(id, hearing_day_id, panel_user_id,is_presiding) values (3, 2, 'panel1-2',null);
+insert into hearing_day_panel(id, hearing_day_id, panel_user_id,is_presiding) values (4, 3, 'panel2-1','true');
+insert into hearing_day_panel(id, hearing_day_id, panel_user_id,is_presiding) values (5, 4, 'panel3-1','false');
 
-insert into hearing_attendee_details(id, hearing_day_id, party_id, party_sub_channel_type) values (81, 61, 'party1-1','subChannel1-1');
-insert into hearing_attendee_details(id, hearing_day_id, party_id, party_sub_channel_type) values (82, 61, 'party1-2','subChannel1-2');
-insert into hearing_attendee_details(id, hearing_day_id, party_id, party_sub_channel_type) values (83, 63, 'party2-1','subChannel2-1');
-insert into hearing_attendee_details(id, hearing_day_id, party_id, party_sub_channel_type) values (84, 64, 'party3-1','subChannel3-1');
+insert into hearing_attendee_details(id, hearing_day_id, party_id, party_sub_channel_type) values (1, 1, 'party1-1','subChannel1-1');
+insert into hearing_attendee_details(id, hearing_day_id, party_id, party_sub_channel_type) values (2, 1, 'party1-2','subChannel1-2');
+insert into hearing_attendee_details(id, hearing_day_id, party_id, party_sub_channel_type) values (3, 3, 'party2-1','subChannel2-1');
+insert into hearing_attendee_details(id, hearing_day_id, party_id, party_sub_channel_type) values (4, 4, 'party3-1','subChannel3-1');
