9c73439e161e9f0d465bf1e478859e91d60e3754
rebeccabaker <rebecca.baker@hmcts.net>
2022-03-24 08:31:49 +0000

    HMAN-78 refactored code

--- src/main/java/uk/gov/hmcts/reform/hmc/config/MessageProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/config/MessageProcessor.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.hmc.exceptions.ListAssistResponseException;
 import uk.gov.hmcts.reform.hmc.exceptions.MalformedMessageException;
 import uk.gov.hmcts.reform.hmc.service.InboundQueueService;
 
@@ -22,6 +23,7 @@ public class MessageProcessor {
     private static final String MESSAGE_TYPE = "message_type";
     public static final String MISSING_MESSAGE_TYPE = "Message is missing custom header message_type";
     public static final String MESSAGE_PARSE_ERROR = "Unable to parse incoming message with id '{}'";
+    public static final String ERROR_MESSAGE_TYPE = "Message of type error received with id '{}'";
 
     public MessageProcessor(ObjectMapper objectMapper, InboundQueueService inboundQueueService) {
         this.objectMapper = objectMapper;
@@ -41,6 +43,9 @@ public class MessageProcessor {
         } catch (JsonProcessingException ex) {
             log.error(MESSAGE_PARSE_ERROR, message.getMessageId(), ex);
             throw new MalformedMessageException(MESSAGE_PARSE_ERROR);
+        } catch (ListAssistResponseException ex) {
+            log.error(ERROR_MESSAGE_TYPE, message.getMessageId());
+            client.abandon(message);
         }
     }
 
