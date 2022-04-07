3783376f506e228685eb6ab494dd68edf1c32f2f
rafalkalita <rkalitainfo@gmail.com>
2022-04-07 16:50:14 +0100

    HMAN-82 fix data - request version to match response version

--- src/integrationTest/resources/sql/put-hearing-actuals.sql
+++ src/integrationTest/resources/sql/put-hearing-actuals.sql
@@ -9,7 +9,7 @@ insert into case_hearing_request (auto_list_flag, hearing_type, required_duratio
 VALUES ( 't'	,'Some hearing type',	60,	'Priority type',	4,'f','f','AB123',null,'ABA1'	,1111222233334444,	'2021-08-10 11:20:00','EXT/REF123',	'https://www.google.com',	'Internal case name','Public case name',	't'	,'CMLC123',	't',	'2021-10-10 00:00:00',	1,	2000000302	,'t'	,	'Some listing comments',	'Some judge',	'2021-11-01 00:00:00',	'2021-11-12 00:00:00');
 
 INSERT INTO hearing_response(hearing_response_id, hearing_id, received_date_time, listing_status, listing_case_status, response_version, request_version)
-VALUES (1, '2000000302', '2021-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '2', '1');
+VALUES (1, '2000000302', '2021-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '1', '1');
 
 INSERT INTO hearing_day_details(hearing_day_id, hearing_response_id, start_date_time, end_date_time, list_assist_session_id, venue_id, room_id)
 VALUES (1, '1', '2022-01-31 11:20:00', '2022-01-31 14:20:00', 'session1-1', 'venue1-1', 'room1-1');
@@ -23,7 +23,7 @@ insert into case_hearing_request (auto_list_flag, hearing_type, required_duratio
 VALUES ( 't'	,'Some hearing type',	60,	'Priority type',	4,'f','f','AB123',null,'ABA1'	,1111222233334444,	'2021-08-10 11:20:00','EXT/REF123',	'https://www.google.com',	'Internal case name','Public case name',	't'	,'CMLC123',	't',	'2021-10-10 00:00:00',	1,	2000001000	,'t'	,	'Some listing comments',	'Some judge',	'2021-11-01 00:00:00',	'2021-11-12 00:00:00');
 
 INSERT INTO hearing_response(hearing_response_id, hearing_id, received_date_time, listing_status, listing_case_status, response_version, request_version)
-VALUES (2, '2000001000', '2021-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '2', '1');
+VALUES (2, '2000001000', '2021-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '1', '1');
 
 INSERT INTO hearing_day_details(hearing_day_id, hearing_response_id, start_date_time, end_date_time, list_assist_session_id, venue_id, room_id)
 VALUES (3, '2', '2022-01-15 11:20:00', '2022-01-15 14:20:00', 'session1-1', 'venue1-1', 'room1-1');
@@ -35,7 +35,7 @@ insert into case_hearing_request (auto_list_flag, hearing_type, required_duratio
 VALUES ( 't'	,'Some hearing type',	60,	'Priority type',	4,'f','f','AB123',null,'ABA1'	,1111222233334444,	'2021-08-10 11:20:00','EXT/REF123',	'https://www.google.com',	'Internal case name','Public case name',	't'	,'CMLC123',	't',	'2021-10-10 00:00:00',	1,	2000001100	,'t'	,	'Some listing comments',	'Some judge',	'2021-11-01 00:00:00',	'2021-11-12 00:00:00');
 
 INSERT INTO hearing_response(hearing_response_id, hearing_id, received_date_time, listing_status, listing_case_status, response_version, request_version)
-VALUES (3, '2000001100', '2021-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '2', '1');
+VALUES (3, '2000001100', '2021-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '1', '1');
 
 INSERT INTO hearing_day_details(hearing_day_id, hearing_response_id, start_date_time, end_date_time, list_assist_session_id, venue_id, room_id)
 VALUES (4, '3', '2022-01-15 11:20:00', '2022-01-15 14:20:00', 'session1-1', 'venue1-1', 'room1-1');
@@ -47,7 +47,7 @@ insert into case_hearing_request (auto_list_flag, hearing_type, required_duratio
 VALUES ( 't'	,'Some hearing type',	60,	'Priority type',	4,'f','f','AB123',null,'ABA1'	,1111222233334444,	'2021-08-10 11:20:00','EXT/REF123',	'https://www.google.com',	'Internal case name','Public case name',	't'	,'CMLC123',	't',	'2021-10-10 00:00:00',	1,	2000001200	,'t'	,	'Some listing comments',	'Some judge',	'2021-11-01 00:00:00',	'2021-11-12 00:00:00');
 
 INSERT INTO hearing_response(hearing_response_id, hearing_id, received_date_time, listing_status, listing_case_status, response_version, request_version)
-VALUES (4, '2000001200', '2021-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '2', '1');
+VALUES (4, '2000001200', '2021-08-10 11:20:00', 'listingStatus1-1', 'caselistingStatus1-1', '1', '1');
 
 INSERT INTO hearing_day_details(hearing_day_id, hearing_response_id, start_date_time, end_date_time, list_assist_session_id, venue_id, room_id)
 VALUES (5, '4', '2022-01-15 11:20:00', '2022-01-15 14:20:00', 'session1-1', 'venue1-1', 'room1-1');
