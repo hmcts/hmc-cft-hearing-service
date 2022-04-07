15c3c8c3855c87d0bff791d84e4a78cbafe61f80
rafalkalita <rkalitainfo@gmail.com>
2022-04-07 14:41:00 +0100

    HMAN-82 removesd Propagation.REQUIRES_NEW

--- src/main/java/uk/gov/hmcts/reform/hmc/repository/ActualHearingRepository.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/repository/ActualHearingRepository.java
@@ -2,14 +2,13 @@ package uk.gov.hmcts.reform.hmc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.hmc.data.ActualHearingEntity;
 import uk.gov.hmcts.reform.hmc.data.HearingResponseEntity;
 
 import java.util.Optional;
 
-@Transactional(propagation = Propagation.REQUIRES_NEW)
+@Transactional
 @Repository
 public interface ActualHearingRepository extends JpaRepository<ActualHearingEntity, Long> {
 
