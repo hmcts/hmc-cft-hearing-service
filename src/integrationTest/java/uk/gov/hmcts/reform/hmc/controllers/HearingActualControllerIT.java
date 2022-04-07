1ed7daeedee3165df60435983ce14bd4f2cb7352
Kiran Yenigala <kiran.yenigala@nihr.ac.uk>
2022-04-07 21:47:18 +0100

    HMAN-125: Access Control implementation : fixed test cases

--- src/integrationTest/java/uk/gov/hmcts/reform/hmc/controllers/HearingActualControllerIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/hmc/controllers/HearingActualControllerIT.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.hmc.controllers;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,17 +13,35 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.reform.hmc.ApplicationParams;
 import uk.gov.hmcts.reform.hmc.BaseTest;
+import uk.gov.hmcts.reform.hmc.data.RoleAssignmentAttributesResource;
+import uk.gov.hmcts.reform.hmc.data.RoleAssignmentResource;
+import uk.gov.hmcts.reform.hmc.data.RoleAssignmentResponse;
 import uk.gov.hmcts.reform.hmc.exceptions.ValidationError;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.reform.hmc.WiremockFixtures.stubReturn200RoleAssignments;
+import static uk.gov.hmcts.reform.hmc.controllers.HearingManagementControllerIT.CASE_TYPE;
+import static uk.gov.hmcts.reform.hmc.controllers.HearingManagementControllerIT.JURISDICTION;
+import static uk.gov.hmcts.reform.hmc.controllers.HearingManagementControllerIT.USER_ID;
+import static uk.gov.hmcts.reform.hmc.service.AccessControlServiceImpl.HEARNING_MANAGER;
+import static uk.gov.hmcts.reform.hmc.service.AccessControlServiceImpl.HEARNING_VIEWER;
 
 
 class HearingActualControllerIT extends BaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(HearingActualControllerIT.class);
+
+    public static final String ROLE_TYPE = "ORGANISATION";
 
     @Autowired
     private MockMvc mockMvc;
@@ -38,9 +58,19 @@ class HearingActualControllerIT extends BaseTest {
     @DisplayName("Get Hearing Actuals")
     class GetHearingActuals {
 
+        @BeforeEach
+        void setUp() {
+            stubRoleAssignments();
+        }
+
         @Test
         @Sql(scripts = {DELETE_HEARING_DATA_SCRIPT, GET_HEARINGS_DATA_SCRIPT})
         void shouldReturn200_WhenHearingExists() throws Exception {
+            stubFor(WireMock.get(urlMatching("/cases/9372710950276233"))
+                        .willReturn(okJson("{\n"
+                                               + "\t\"jurisdiction\": \"Jurisdiction1\",\n"
+                                               + "\t\"case_type\": \"CaseType1\"\n"
+                                               + "}")));
             mockMvc.perform(get(url + "/2000000000")
                                 .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().is(200))
@@ -63,6 +93,30 @@ class HearingActualControllerIT extends BaseTest {
                 .andExpect(status().is(400))
                 .andExpect(jsonPath("$.errors", hasItem(ValidationError.INVALID_HEARING_ID_DETAILS)))
                 .andReturn();
+        }
+
+        private void stubRoleAssignments() {
+            RoleAssignmentResource resource = new RoleAssignmentResource();
+            resource.setRoleName(HEARNING_MANAGER);
+            resource.setRoleType(ROLE_TYPE);
+            RoleAssignmentAttributesResource attributesResource = new RoleAssignmentAttributesResource();
+            attributesResource.setCaseType(Optional.of(CASE_TYPE));
+            attributesResource.setJurisdiction(Optional.of(JURISDICTION));
+            resource.setAttributes(attributesResource);
+
+            RoleAssignmentResource hearingViewer = new RoleAssignmentResource();
+            hearingViewer.setRoleName(HEARNING_VIEWER);
+            hearingViewer.setRoleType(ROLE_TYPE);
+            RoleAssignmentAttributesResource hearingViewerResource = new RoleAssignmentAttributesResource();
+            hearingViewerResource.setCaseType(Optional.of(CASE_TYPE));
+            hearingViewerResource.setJurisdiction(Optional.of(JURISDICTION));
+            hearingViewer.setAttributes(hearingViewerResource);
+            List<RoleAssignmentResource> roleAssignmentList = new ArrayList<>();
+            roleAssignmentList.add(resource);
+            roleAssignmentList.add(hearingViewer);
+            RoleAssignmentResponse response = new RoleAssignmentResponse();
+            response.setRoleAssignments(roleAssignmentList);
+            stubReturn200RoleAssignments(USER_ID, response);
         }
     }
 }
