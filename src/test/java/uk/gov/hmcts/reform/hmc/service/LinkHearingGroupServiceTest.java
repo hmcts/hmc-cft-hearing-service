e6ef6545b0cf7593873932b78551803558e1da2c
rebeccabaker <rebecca.baker@hmcts.net>
2022-04-08 12:44:14 +0100

    HMAN-93 checkstyle

--- src/test/java/uk/gov/hmcts/reform/hmc/service/LinkHearingGroupServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/service/LinkHearingGroupServiceTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
