58f03aa68d34c80d8bcc89d44595242efa4d3042
KleoG <kleogeorgiou@hotmail.com>
2022-04-08 11:44:03 +0100

    HMAN-142 - update EntityCommunication to take into account multiple items

--- src/main/java/uk/gov/hmcts/reform/hmc/model/hmi/EntityCommunication.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/model/hmi/EntityCommunication.java
@@ -3,13 +3,11 @@ package uk.gov.hmcts.reform.hmc.model.hmi;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.List;
-
 @Builder
 @Data
 public class EntityCommunication {
 
-    private List<String> entityCommunicationDetails;
+    private String entityCommunicationDetails;
 
     private String entityCommunicationType;
 
--- src/test/java/uk/gov/hmcts/reform/hmc/helper/hmi/CommunicationsMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/helper/hmi/CommunicationsMapperTest.java
@@ -24,13 +24,46 @@ class CommunicationsMapperTest {
         partyDetails.setIndividualDetails(individualDetails);
         EntityCommunication entityCommunication = EntityCommunication.builder()
             .entityCommunicationType(PHONE_TYPE)
-            .entityCommunicationDetails(List.of("phoneNumber"))
+            .entityCommunicationDetails("phoneNumber")
             .build();
         EntityCommunication entityCommunicationTwo = EntityCommunication.builder()
             .entityCommunicationType(EMAIL_TYPE)
-            .entityCommunicationDetails(List.of("email"))
+            .entityCommunicationDetails("email")
             .build();
         List<EntityCommunication> expectedCommunications = Arrays.asList(entityCommunicationTwo, entityCommunication);
+        CommunicationsMapper communicationsMapper = new CommunicationsMapper();
+        List<EntityCommunication> actualCommunications = communicationsMapper.getCommunications(partyDetails);
+        assertEquals(expectedCommunications, actualCommunications);
+    }
+
+    @Test
+    void shouldMapWhenPartyDetailsContainsMultipleEmailAndPhone() {
+        PartyDetails partyDetails = new PartyDetails();
+        IndividualDetails individualDetails = new IndividualDetails();
+        individualDetails.setHearingChannelPhone(List.of("phoneNumber1", "phoneNumber2"));
+        individualDetails.setHearingChannelEmail(List.of("email1", "email2"));
+        partyDetails.setIndividualDetails(individualDetails);
+        EntityCommunication entityCommunicationPhoneOne = EntityCommunication.builder()
+            .entityCommunicationType(PHONE_TYPE)
+            .entityCommunicationDetails("phoneNumber1")
+            .build();
+        EntityCommunication entityCommunicationPhoneTwo = EntityCommunication.builder()
+            .entityCommunicationType(PHONE_TYPE)
+            .entityCommunicationDetails("phoneNumber2")
+            .build();
+        EntityCommunication entityCommunicationEmailOne = EntityCommunication.builder()
+            .entityCommunicationType(EMAIL_TYPE)
+            .entityCommunicationDetails("email1")
+            .build();
+        EntityCommunication entityCommunicationEmailTwo = EntityCommunication.builder()
+            .entityCommunicationType(EMAIL_TYPE)
+            .entityCommunicationDetails("email2")
+            .build();
+        List<EntityCommunication> expectedCommunications = Arrays.asList(
+                                                                entityCommunicationEmailOne,
+                                                                entityCommunicationEmailTwo,
+                                                                entityCommunicationPhoneOne,
+                                                                entityCommunicationPhoneTwo);
         CommunicationsMapper communicationsMapper = new CommunicationsMapper();
         List<EntityCommunication> actualCommunications = communicationsMapper.getCommunications(partyDetails);
         assertEquals(expectedCommunications, actualCommunications);
