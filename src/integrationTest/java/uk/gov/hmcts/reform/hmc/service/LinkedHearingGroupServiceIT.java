8ab4454c96651b76a1033c3265bc0fc664c19895
padmapriyanalam <padmapriya.nalam@gmail.com>
2022-04-10 13:54:10 +0100

    HMAN-97 CVE suppressions

--- src/integrationTest/java/uk/gov/hmcts/reform/hmc/service/LinkedHearingGroupServiceIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/hmc/service/LinkedHearingGroupServiceIT.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.hmc.service;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
