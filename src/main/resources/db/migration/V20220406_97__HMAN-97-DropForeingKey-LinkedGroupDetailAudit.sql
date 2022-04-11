954b2b502ca1b4690c14711419d4b0508a27cbb7
padmapriyanalam <padmapriya.nalam@gmail.com>
2022-04-11 11:23:08 +0100

    HMAN-97 Fix linked_group_details_audit sequence

--- src/main/resources/db/migration/V20220406_97__HMAN-97-DropForeingKey-LinkedGroupDetailAudit.sql
+++ src/main/resources/db/migration/V20220406_97__HMAN-97-DropForeingKey-LinkedGroupDetailAudit.sql
@@ -1,5 +1,7 @@
 ALTER TABLE ONLY public.linked_group_details_audit
-DROP CONSTRAINT fk_linked_group_details_audit_linked_group_id;
+DROP CONSTRAINT IF EXISTS fk_linked_group_details_audit_linked_group_id;
 
 ALTER TABLE ONLY public.linked_hearing_details_audit
-DROP CONSTRAINT fk_linked_hearing_details_audit_linked_group_id;
+DROP CONSTRAINT IF EXISTS fk_linked_hearing_details_audit_linked_group_id;
+
+ALTER TABLE ONLY public.linked_group_details_audit ALTER COLUMN linked_group_details_audit_id SET DEFAULT nextval('public.linked_group_details_audit_id_seq'::regclass);
