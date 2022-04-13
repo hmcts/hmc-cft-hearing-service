9114eb91bd53eb66d88149c35bc3c75322bf34fd
padmapriyanalam <padmapriya.nalam@gmail.com>
2022-04-13 09:58:20 +0100

    HMAN-97 Code review fix

--- src/main/java/uk/gov/hmcts/reform/hmc/service/LinkedHearingGroupServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/service/LinkedHearingGroupServiceImpl.java
@@ -1,10 +1,10 @@
 package uk.gov.hmcts.reform.hmc.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import lombok.extern.slf4j.Slf4j;
 import uk.gov.hmcts.reform.hmc.data.HearingEntity;
 import uk.gov.hmcts.reform.hmc.data.HearingResponseEntity;
 import uk.gov.hmcts.reform.hmc.data.LinkedGroupDetails;
