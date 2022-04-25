01c572678d42904333486def5c943d0083113559
KleoG <kleogeorgiou@hotmail.com>
2022-04-25 09:45:13 +0100

    HMAN-163 - Fit IT tests

--- src/test/java/uk/gov/hmcts/reform/hmc/helper/HmiHearingResponseMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/helper/HmiHearingResponseMapperTest.java
@@ -418,6 +418,13 @@ class HmiHearingResponseMapperTest {
         hearingJoh.setIsPresiding(true);
         hearing.setHearingJohs(new ArrayList<>(List.of(hearingJoh)));
 
+        HearingSession hearingSession = new HearingSession();
+        hearingSession.setHearingVenue(hearingVenue);
+        hearingSession.setHearingRoom(hearingRoom);
+        hearingSession.setHearingStartTime(LocalDateTime.parse("2021-08-10T12:20:00"));
+        hearingSession.setHearingEndTime(LocalDateTime.parse("2021-08-10T12:20:00"));
+        hearing.setHearingSessions(new ArrayList<>(List.of(hearingSession)));
+
         hearingResponse.setHearing(hearing);
         return hearingResponse;
     }
