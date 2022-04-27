0672f4ab1fdf876f3ce5c449386c2ac93f2396b3
rebeccabaker <rebecca.baker@hmcts.net>
2022-04-27 08:14:14 +0100

    HMAN-175 Update HearingCode enum to reference numbers

--- src/main/java/uk/gov/hmcts/reform/hmc/client/hmi/HearingCode.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/client/hmi/HearingCode.java
@@ -3,12 +3,35 @@ package uk.gov.hmcts.reform.hmc.client.hmi;
 import java.util.Arrays;
 
 public enum HearingCode {
-    LISTED,
-    PENDING_RELISTING,
-    EXCEPTION,
-    CLOSED;
+    LISTED(100, "LISTED"),
+    PENDING_RELISTING(6, "PENDING_RELISTING"),
+    CLOSED(8, "CLOSED"),
+    EXCEPTION(0, "EXCEPTION"),
+    ;
 
-    public static boolean isValid(String status) {
-        return Arrays.stream(values()).anyMatch(enumStatus -> enumStatus.name().equals(status));
+    private int number;
+    private String label;
+
+    HearingCode(int number, String label) {
+        this.number = number;
+        this.label = label;
+    }
+
+    public static boolean isValidNumber(int number) {
+        return Arrays.stream(values()).anyMatch(eachType -> (eachType.number == number));
+    }
+
+    public static HearingCode getByLabel(String name) {
+        return Arrays.stream(HearingCode.values())
+            .filter(eachLinkType -> eachLinkType.label == name).findAny().orElse(null);
+    }
+
+    public static HearingCode getByNumber(String number) {
+        return Arrays.stream(HearingCode.values())
+            .filter(eachLinkType -> eachLinkType.number == Integer.valueOf(number)).findAny().orElse(null);
+    }
+
+    public static int getNumber(HearingCode code) {
+        return code.number;
     }
 }
