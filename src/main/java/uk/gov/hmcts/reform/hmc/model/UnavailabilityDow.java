a23622b71e5e5d2b8a93ea0818b0bad66b71a4d5
Gareth Sullivan <gareth.sullivan@cgi.com>
2022-04-27 13:45:09 +0100

    HMAN-165 - Document enum values in swagger ui pages

--- src/main/java/uk/gov/hmcts/reform/hmc/model/UnavailabilityDow.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/model/UnavailabilityDow.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.hmc.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.hmc.validator.CapitalizedEnumPattern;
@@ -11,6 +12,7 @@ public class UnavailabilityDow {
 
     @JsonProperty("DOW")
     @CapitalizedEnumPattern(enumClass = DayOfWeekUnavailable.class, fieldName = "dow")
+    @ApiModelProperty(allowableValues = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday")
     private String dow;
 
     @JsonProperty("DOWUnavailabilityType")
