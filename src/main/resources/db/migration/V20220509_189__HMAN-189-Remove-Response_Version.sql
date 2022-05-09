912e01c00251f8d0a8df75711747ca6773c37ab3
markdathorne <mark.dathorne@cgi.com>
2022-05-09 14:21:52 +0100

    HMAN-189 Drop response version column from hearing_response table

--- src/main/resources/db/migration/V20220509_189__HMAN-189-Remove-Response_Version.sql
+++ src/main/resources/db/migration/V20220509_189__HMAN-189-Remove-Response_Version.sql
@@ -1,5 +1,5 @@
 --
 -- DROP COLUMN hearing_response.response_version
 --
-ALTER TABLE ONLY public.hearing_response DROP COLUMN response_version;
+ALTER TABLE ONLY public.hearing_response DROP COLUMN IF EXISTS response_version;
 
