d906f8c1e15ee4b92aefc3e3b48a8652203d5d1a
marvincudjoe <18750678+marvincudjoe@users.noreply.github.com>
2022-06-01 11:42:29 +0100

    Revert "HMAN-221 missing validation on hearingWindow"
    This reverts commit 04e6f15e740c271f1b3c01a00f6f80028cc20ba1.

--- src/main/resources/db/migration/V20220525_228__HMAN-228-CreateHearingChannelsTable.sql
+++ src/main/resources/db/migration/V20220525_228__HMAN-228-CreateHearingChannelsTable.sql
@@ -16,8 +16,6 @@ CREATE SEQUENCE public.hearing_channels_id_seq
 
 ALTER SEQUENCE public.hearing_channels_id_seq OWNED BY public.hearing_channels.hearing_channels_id;
 
-ALTER TABLE ONLY public.hearing_channels ALTER COLUMN hearing_channels_id SET DEFAULT nextval('public.hearing_channels_id_seq'::regclass);
-
 ALTER TABLE ONLY public.hearing_channels
     ADD CONSTRAINT fk_hearing_channels_case_hearing_request FOREIGN KEY (case_hearing_id) REFERENCES public.case_hearing_request(case_hearing_id);
 
