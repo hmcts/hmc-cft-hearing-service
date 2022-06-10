6421c980cebcd90e72eb9c730796df926d98501a
KleoG <kleogeorgiou@hotmail.com>
2022-06-10 10:52:29 +0100

    HMAN-271 - Fix checkstyles

--- src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/CftHearingServiceTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/CftHearingServiceTestAutomationAdapter.java
@@ -3,11 +3,8 @@ package uk.gov.hmcts.reform.hmc.hmi.befta;
 import uk.gov.hmcts.befta.BeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultBeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
-import uk.gov.hmcts.befta.data.UserData;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
 import uk.gov.hmcts.befta.util.BeftaUtils;
-
-import java.util.concurrent.ExecutionException;
 
 public class CftHearingServiceTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
