aef81353f742d7e217a7e031ebb6cdaaa62b617f
markdathorne <mark.dathorne@cgi.com>
2022-06-14 16:25:17 +0100

    HMAN-275 Correct field definition update script

--- src/main/resources/db/migration/V20220614_275__HMAN-275-OptionalFieldsNowMandatoryNotNull.sql
+++ src/main/resources/db/migration/V20220614_275__HMAN-275-OptionalFieldsNowMandatoryNotNull.sql
@@ -1,9 +1,9 @@
-ALTER TABLE ONLY public.actual_hearing ALTER COLUMN actual_hearing_type SET NOT NULL;
+ALTER TABLE ONLY public.actual_hearing ALTER COLUMN actual_hearing_type DROP NOT NULL;
 
-ALTER TABLE ONLY public.actual_hearing ALTER COLUMN hearing_final_flag SET NOT NULL;
+ALTER TABLE ONLY public.actual_hearing ALTER COLUMN actual_hearing_is_final_flag DROP NOT NULL;
 
-ALTER TABLE ONLY public.actual_hearing ALTER COLUMN hearing_result SET NOT NULL;
+ALTER TABLE ONLY public.actual_hearing ALTER COLUMN hearing_result_type DROP NOT NULL;
 
-ALTER TABLE ONLY public.actual_hearing ALTER COLUMN hearing_result_reason_type SET NOT NULL;
+ALTER TABLE ONLY public.actual_hearing ALTER COLUMN hearing_result_reason_type DROP NOT NULL;
 
-ALTER TABLE ONLY public.actual_hearing ALTER COLUMN hearing_result_date SET NOT NULL;
+ALTER TABLE ONLY public.actual_hearing ALTER COLUMN hearing_result_date DROP NOT NULL;
