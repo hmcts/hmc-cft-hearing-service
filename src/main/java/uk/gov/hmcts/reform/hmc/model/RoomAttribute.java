7f4a9285138f8ab941d4238d20f110d1b1c6bd18
Dan Lysiak <daniel.lysiak1@hmcts.net>
2022-06-21 13:19:23 +0100

    HMAN-269 - Load roomAttributes on startup

--- src/main/java/uk/gov/hmcts/reform/hmc/model/RoomAttribute.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/model/RoomAttribute.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.hmc.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,4 +13,19 @@ public class RoomAttribute {
     private String roomAttributeName;
     private String reasonableAdjustmentCode;
     private boolean facility;
+
+    @JsonCreator
+    public RoomAttribute(@JsonProperty(value = "roomAttributeCode", required = true)
+                                 String roomAttributeCode,
+                         @JsonProperty(value = "roomAttributeName", required = true)
+                                 String roomAttributeName,
+                         @JsonProperty(value = "reasonableAdjustmentCode", required = true)
+                                 String reasonableAdjustmentCode,
+                         @JsonProperty(value = "facility", required = true)
+                                 boolean facility) {
+        this.roomAttributeCode = roomAttributeCode;
+        this.roomAttributeName = roomAttributeName;
+        this.reasonableAdjustmentCode = reasonableAdjustmentCode;
+        this.facility = facility;
+    }
 }
