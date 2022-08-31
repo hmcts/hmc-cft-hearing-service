af728c204d630a3fd3ec5efb1d15529e1f5e2075
rebeccabaker <rebecca.baker@hmcts.net>
2022-08-31 09:51:04 +0100

    HMAN-405 HMAN-407 Set String to max length 60

--- src/main/java/uk/gov/hmcts/reform/hmc/model/HearingDaySchedule.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/model/HearingDaySchedule.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.hmc.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.hmc.exceptions.ValidationError;
 
 import java.time.LocalDateTime;
 import java.util.List;
