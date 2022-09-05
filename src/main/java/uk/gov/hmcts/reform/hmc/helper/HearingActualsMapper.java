419be9102d56d5d813e8de837b5b67dbddfbe61a
padmapriyanalam <padmapriya.nalam@gmail.com>
2022-09-05 14:08:15 +0100

    HMAN-414 HearingActuals hearingStartTime and hearingEndTime need to be made optional

--- src/main/java/uk/gov/hmcts/reform/hmc/helper/HearingActualsMapper.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/helper/HearingActualsMapper.java
@@ -73,16 +73,6 @@ public class HearingActualsMapper {
 
     private void setNotRequired(ActualHearingDayEntity actualHearingDayEntity, ActualHearingDay actualHearingDay) {
         actualHearingDayEntity.setNotRequired(actualHearingDay.getNotRequired());
-        if (null == actualHearingDay.getNotRequired() || !actualHearingDay.getNotRequired()) {
-            if (null == actualHearingDay.getHearingStartTime()) {
-                throw new BadRequestException(
-                    String.format("005 missing hearingStartTime for %s", actualHearingDay.getHearingDate()));
-            }
-            if (null == actualHearingDay.getHearingEndTime()) {
-                throw new BadRequestException(
-                    String.format("006 missing hearingEndTime for %s", actualHearingDay.getHearingDate()));
-            }
-        }
     }
 
     private List<ActualHearingDayPausesEntity> toActualHearingDayPausesEntities(
