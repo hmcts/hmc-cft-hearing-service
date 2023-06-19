b37a67dbb8d90e279a470b2c2bfc7919aacdba82
padmapriyanalam <padmapriya.nalam@gmail.com>
2023-06-19 15:23:52 +0100

    HMAN-560:  logger info statements when hearing is updated to Exception

--- src/integrationTest/java/uk/gov/hmcts/reform/hmc/config/MessageProcessorIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/hmc/config/MessageProcessorIT.java
@@ -263,10 +263,11 @@ class MessageProcessorIT extends BaseTest {
         messageProcessor.processMessage(errorJsonNode, messageContext);
 
         List<ILoggingEvent> logsList = listAppender.list;
-        assertEquals(2, logsList.size());
+        assertEquals(3, logsList.size());
         assertEquals(Level.INFO, logsList.get(0).getLevel());
         assertEquals("Message of type ERROR received", logsList.get(0).getMessage());
-        assertEquals(Level.ERROR, logsList.get(1).getLevel());
+        assertEquals(Level.INFO, logsList.get(1).getLevel());
+        assertEquals(Level.ERROR, logsList.get(2).getLevel());
         assertEquals("Hearing id: 2000000000 updated to status Exception", logsList.get(1).getMessage());
 
         List<ILoggingEvent> logsListMessageProcessor = listAppenderMessageProcessor.list;
