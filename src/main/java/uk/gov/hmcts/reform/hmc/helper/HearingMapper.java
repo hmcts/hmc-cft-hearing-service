40dce26d54b60de9a8f481869ca0764ba36e0a82
padmapriyanalam <padmapriya.nalam@gmail.com>
2023-09-04 10:13:52 +0100

    HMAN-550-Ensure that hmctsDeploymentId property is set on all outgoing messages

--- src/main/java/uk/gov/hmcts/reform/hmc/helper/HearingMapper.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/helper/HearingMapper.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.hmc.helper;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.hmc.data.CaseHearingRequestEntity;
