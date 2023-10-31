32fc4acd526263b1a014f167302d471ab31c4e81
padmapriyanalam <padmapriya.nalam@gmail.com>
2023-10-31 12:49:08 +0000

    HMAN-529 dummy build

--- src/integrationTest/java/uk/gov/hmcts/reform/hmc/controllers/HearingManagementControllerIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/hmc/controllers/HearingManagementControllerIT.java
@@ -886,7 +886,6 @@ class HearingManagementControllerIT extends BaseTest {
             .andReturn();
     }
 
-
     @Test
     @Sql(scripts = {DELETE_HEARING_DATA_SCRIPT, INSERT_CASE_HEARING_DATA_SCRIPT})
     void shouldReturn201WhenUpdateHearingRequestIsValid() throws Exception {
