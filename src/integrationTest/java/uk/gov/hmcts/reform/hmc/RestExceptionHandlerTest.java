192bac079f6c44444a1c989b7d1ec9bf44684c74
padmapriyanalam <padmapriya.nalam@gmail.com>
2024-01-25 11:08:15 +0000

    HMAN-620- Unused import

--- src/integrationTest/java/uk/gov/hmcts/reform/hmc/RestExceptionHandlerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/hmc/RestExceptionHandlerTest.java
@@ -15,7 +15,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import uk.gov.hmcts.reform.hmc.exceptions.BadRequestException;
