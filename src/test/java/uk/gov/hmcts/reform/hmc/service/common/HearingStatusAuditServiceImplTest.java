f6601b2023583fe353e76c231c7b6c2ee0ec7010
padmapriyanalam <padmapriya.nalam@gmail.com>
2024-02-19 16:08:58 +0000

    HMAN-578- fix unit tests

--- src/test/java/uk/gov/hmcts/reform/hmc/service/common/HearingStatusAuditServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/service/common/HearingStatusAuditServiceImplTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.hmc.service.common;
 
-import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -8,31 +7,16 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import uk.gov.hmcts.reform.hmc.data.SecurityUtils;
-import uk.gov.hmcts.reform.hmc.domain.model.enums.PutHearingStatus;
-import uk.gov.hmcts.reform.hmc.exceptions.BadRequestException;
 import uk.gov.hmcts.reform.hmc.helper.HearingStatusAuditMapper;
-import uk.gov.hmcts.reform.hmc.model.HearingResponse;
-import uk.gov.hmcts.reform.hmc.model.HearingStatusAudit;
-import uk.gov.hmcts.reform.hmc.repository.CaseHearingRequestRepository;
 import uk.gov.hmcts.reform.hmc.repository.HearingStatusAuditRepository;
-import uk.gov.hmcts.reform.hmc.service.UnNotifiedHearingServiceImpl;
 import uk.gov.hmcts.reform.hmc.utils.TestingUtil;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static uk.gov.hmcts.reform.hmc.constants.Constants.HMC_TARGET;
 import static uk.gov.hmcts.reform.hmc.constants.Constants.HMI_TARGET;
-import static uk.gov.hmcts.reform.hmc.exceptions.ValidationError.INVALID_HMCTS_SERVICE_CODE;
 
 public class HearingStatusAuditServiceImplTest {
 
@@ -63,10 +47,10 @@ public class HearingStatusAuditServiceImplTest {
         @Test
         void shouldSaveAuditTriageDetails() {
             given(securityUtils.getServiceNameFromS2SToken(HMC_TARGET)).willReturn(HMC_TARGET);
-            given(hearingStatusAuditMapper.modelToEntity(TestingUtil.hearingStatusAudit())).willReturn
-                (TestingUtil.hearingStatusAuditEntity());
-            given(hearingStatusAuditRepository.save(TestingUtil.hearingStatusAuditEntity())).willReturn
-                (TestingUtil.hearingStatusAuditEntity());
+            given(hearingStatusAuditMapper.modelToEntity(TestingUtil.hearingStatusAudit())).willReturn(
+                TestingUtil.hearingStatusAuditEntity());
+            given(hearingStatusAuditRepository.save(TestingUtil.hearingStatusAuditEntity())).willReturn(
+                TestingUtil.hearingStatusAuditEntity());
             hearingStatusAuditService. saveAuditTriageDetails("ABA1","2000000000",
                                                               "HEARING_REQUESTED", LocalDateTime.now(),
                                                               "create-hearing- request",HMC_TARGET,
