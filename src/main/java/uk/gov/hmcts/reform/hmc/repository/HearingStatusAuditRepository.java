a14f4fa0be97ae109e33cd90fe1a01386ba85c2a
padmapriyanalam <padmapriya.nalam@gmail.com>
2024-03-19 11:59:13 +0000

    HMAN-578 IT test checkstyle issue

--- src/main/java/uk/gov/hmcts/reform/hmc/repository/HearingStatusAuditRepository.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/repository/HearingStatusAuditRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import uk.gov.hmcts.reform.hmc.data.HearingEntity;
 import uk.gov.hmcts.reform.hmc.data.HearingStatusAuditEntity;
 
 import java.util.List;
