ea62f25445a6bb87fa5fa307e4687cb1a3d0c0fb
Antony Leons <antony@leons.dev>
2024-04-08 07:36:00 +0100

    add json field

--- src/main/resources/db/migration/V20240129_572__HMAN-572-PendingRequests.sql
+++ src/main/resources/db/migration/V20240129_572__HMAN-572-PendingRequests.sql
@@ -6,4 +6,5 @@ CREATE TABLE public.pending_requests (
                          last_tried_date_time timestamp without time zone not null,
                          status varchar(20) not null,
                          incident_flag boolean
+                         message json,
 );
