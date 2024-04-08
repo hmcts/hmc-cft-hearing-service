8025fa93e62a16e3f7ecb6472f71550296b9dcdb
Antony Leons <antony@leons.dev>
2024-04-08 07:48:04 +0100

    add repo

---
+++ src/main/java/uk/gov/hmcts/reform/hmc/data/PendingRequestEntity.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.hmc.data;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@Table(name = "pending_requests")
+@Entity
+@Data
+@NoArgsConstructor
+public class PendingRequestEntity implements Serializable {
+
+    private static final long serialVersionUID = -5832580267716907071L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hearing_id")
+    private Long hearingId;
+
+    @Column(name = "version_number", nullable = false)
+    private Integer versionNumber;
+
+    @Column(name = "submitted_date_time", nullable = false)
+    private Timestamp submittedDateTime;
+
+    @Column(name = "retry_count", nullable = false)
+    private Integer retryCount;
+
+    @Column(name = "last_tried_date_time", nullable = false)
+    private Timestamp lastTriedDateTime;
+
+    @Column(name = "status", nullable = false, length = 20)
+    private String status;
+
+    @Column(name = "incident_flag")
+    private Boolean incidentFlag;
+
+    @Column(name = "message")
+    private String extraData;
+}
---
+++ src/main/java/uk/gov/hmcts/reform/hmc/repository/PendingRequestRepository.java
@@ -0,0 +1,49 @@
+package uk.gov.hmcts.reform.hmc.repository;
+
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.reform.hmc.data.PendingRequestEntity;
+
+import javax.persistence.LockModeType;
+import java.util.List;
+
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+@Repository
+public interface PendingRequestRepository extends CrudRepository<PendingRequestEntity, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query(value = "SELECT * FROM pending_requests WHERE status = 'PENDING' AND (last_tried_date_time IS NULL OR last_tried_date_time < NOW() - INTERVAL '15' MINUTE) ORDER BY submitted_date_time ASC LIMIT 1", nativeQuery = true)
+    PendingRequestEntity findOldestPendingRequestForProcessing();
+
+    @Modifying
+    @Query("UPDATE PendingRequestEntity SET status = 'PROCESSING' WHERE id = :id")
+    void markRequestAsProcessing(Long id);
+
+    @Modifying
+    @Query("UPDATE PendingRequestEntity SET status = 'PENDING', retryCount = :retryCount + 1, last_tried_date_time = CURRENT_TIMESTAMP WHERE id = :id")
+    void markRequestAsPendingAndBumpRetryCount(Long id);
+
+    @Modifying
+    @Query("UPDATE PendingRequestEntity SET status = 'COMPLETED' WHERE id = :id")
+    void markRequestAsCompleted(Long id);
+
+    @Modifying
+    @Query("UPDATE PendingRequestEntity SET status = 'EXCEPTION' WHERE id = :id")
+    void markRequestAsException(Long id);
+
+    @Query("SELECT pr FROM PendingRequestEntity pr WHERE pr.submittedDateTime < :CURRENT_TIMESTAMP - INTERVAL '1' DAY AND pr.incidentFlag = false")
+    List<PendingRequestEntity> findRequestsForEscalation();
+
+    @Modifying
+    @Query("UPDATE PendingRequestEntity SET incidentFlag = true WHERE submittedDateTime < CURRENT_TIMESTAMP - INTERVAL '1' DAY AND incidentFlag = false")
+    void identifyRequestsForEscalation();
+
+    @Modifying
+    @Query("DELETE FROM PendingRequestEntity WHERE status = 'COMPLETED' AND submittedDateTime < CURRENT_TIMESTAMP - INTERVAL '30' DAY")
+    void deleteCompletedRecords();
+}
