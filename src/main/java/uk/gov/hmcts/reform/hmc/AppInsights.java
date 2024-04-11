f79881d850ab8dbfa640e646796825961bd28b1f
Antony Leons <antony@leons.dev>
2024-04-11 15:29:21 +0100

    add trackDependency

---
+++ src/main/java/uk/gov/hmcts/reform/hmc/AppInsights.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.hmc;
+
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.Duration;
+import com.microsoft.applicationinsights.telemetry.RequestTelemetry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppInsights {
+    private static final String MODULE = "HMC_HEARING";
+    private final TelemetryClient telemetry;
+
+    @Autowired
+    public AppInsights(TelemetryClient telemetry) {
+        this.telemetry = telemetry;
+    }
+
+    public void trackRequest(long duration, boolean success) {
+        RequestTelemetry rt = new RequestTelemetry();
+        rt.setName(MODULE);
+        rt.setSuccess(success);
+        rt.setDuration(new Duration(duration));
+        telemetry.trackRequest(rt);
+    }
+
+    public void trackException(Exception e) {
+        telemetry.trackException(e);
+    }
+
+    public void trackDependency(String dependencyName, String commandName, long duration, boolean success) {
+        telemetry.trackDependency(dependencyName, commandName, new Duration(duration), success);
+    }
+}
