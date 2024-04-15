fb2c23c09811374f4404bc6a585ea700fa4d6bc6
Antony Leons <antony@leons.dev>
2024-04-15 10:47:13 +0100

    Update V20240129_572__HMAN-572-PendingRequests.sql
    Update V20240129_572__HMAN-572-PendingRequests.sql

--- src/main/resources/db/migration/V20240129_572__HMAN-572-PendingRequests.sql
+++ src/main/resources/db/migration/V20240129_572__HMAN-572-PendingRequests.sql
@@ -5,6 +5,6 @@ CREATE TABLE public.pending_requests (
                          retry_count integer not null,
                          last_tried_date_time timestamp without time zone not null,
                          status varchar(20) not null,
-                         incident_flag boolean
-                         message json,
+                         incident_flag boolean,
+                         message json
 );
