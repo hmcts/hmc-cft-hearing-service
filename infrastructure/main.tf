09e2d75e419e4fcdc916ba0547c39c4638d4e2a9
Muhammad <muhammad.umerji@hmcts.net>
2024-04-15 17:09:59 +0100

    Adding imports to import file

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,11 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-import {
-  id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/hmc-cft-hearing-service-postgres-db-v15-data-aat"
-  to = "module.postgresql_v15_replica.azurerm_resource_group.rg[0]"
-}
-
 locals {
   app_full_name = "${var.product}-${var.component}"
 
