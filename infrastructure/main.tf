2dd6e26ebfcf04a92e070179fe2cc3f36c5863bd
Dan Lysiak <danlysiak@hotmail.com>
2024-04-16 14:58:41 +0100

    Try moving import

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -162,3 +162,8 @@ module "postgresql_v15_replica" {
   source_server_id = var.primary_server_id
 
 }
+
+import {
+  id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/hmc-cft-hearing-service-postgres-db-v15-data-aat"
+  to = module.postgresql_v15_replica.azurerm_resource_group.rg[0]
+}
