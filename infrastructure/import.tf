3ef65691e0672edb76c932dc87b1fed741aaeba6
Muhammad <muhammad.umerji@hmcts.net>
2024-04-17 14:36:33 +0100

    Appending replica db name

--- infrastructure/import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/hmc-cft-hearing-service-postgres-db-v15-data-aat"
-  to = module.postgresql_v15_replica[0].azurerm_resource_group.rg[0]
-}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -155,7 +155,7 @@ module "postgresql_v15_replica" {
   ]
   pgsql_version    = "15"
   product          = var.product
-  name             = "${local.app_full_name}-postgres-db-v15"
+  name             = "${local.app_full_name}-postgres-db-v15-replica"
   pgsql_sku        = var.pgsql_sku
   pgsql_storage_mb = var.pgsql_storage_mb
   create_mode      = "Replica"
