9444ca4ab7641c89e218bdb60df5eb6c92701d15
padmapriyanalam <padmapriya.nalam@gmail.com>
2024-04-24 14:53:54 +0100

    HMAN-628 build failure issues

--- src/integrationTest/resources/sql/unNotified-hearings-request.sql
+++ src/integrationTest/resources/sql/unNotified-hearings-request.sql
@@ -2,6 +2,7 @@ insert into hearing ( hearing_id, status) values ('2100000000', 'HEARING_REQUEST
 insert into hearing ( hearing_id, status) values ('2100000001', 'HEARING_REQUESTED');
 insert into hearing ( hearing_id, status) values ('2100000003', 'HEARING_REQUESTED');
 insert into hearing ( hearing_id, status) values ('2100000004', 'HEARING_REQUESTED');
+insert into hearing ( hearing_id, status) values ('2100000005', 'CANCELLED');
 
 insert into case_hearing_request (
  auto_list_flag, hearing_type, required_duration_in_minutes, hearing_priority_type, number_of_physical_attendees, hearing_in_welsh_flag, private_hearing_required_flag, lead_judge_contract_type, first_date_time_of_hearing_must_be, hmcts_service_code, case_reference, hearing_request_received_date_time, external_case_reference, case_url_context_path, hmcts_internal_case_name, public_case_name, additional_security_required_flag, owning_location_id, case_restricted_flag, case_sla_start_date, hearing_request_version, hearing_id, interpreter_booking_required_flag,  listing_comments, requester, hearing_window_start_date_range, hearing_window_end_date_range)
@@ -15,6 +16,9 @@ values ( 't'	,'hearingType1',	60,	'Priority type1',	4,'f','f','AAA2',null,'AAA2'
 insert into case_hearing_request (
  auto_list_flag, hearing_type, required_duration_in_minutes, hearing_priority_type, number_of_physical_attendees, hearing_in_welsh_flag, private_hearing_required_flag, lead_judge_contract_type, first_date_time_of_hearing_must_be, hmcts_service_code, case_reference, hearing_request_received_date_time, external_case_reference, case_url_context_path, hmcts_internal_case_name, public_case_name, additional_security_required_flag, owning_location_id, case_restricted_flag, case_sla_start_date, hearing_request_version, hearing_id, interpreter_booking_required_flag,  listing_comments, requester, hearing_window_start_date_range, hearing_window_end_date_range)
 values ( 't'	,'hearingType1',	60,	'Priority type1',	4,'f','f','AAA2',null,'AAA2'	,9372710950276233,	'2021-08-10 11:20:00','EXT/REF123',	'https://www.google.com',	'Internal case name','Public case name',	't'	,'CMLC123',	't',	'2021-10-10 00:00:00',	2,	2100000004	,'t'	,	'Some listing comments1',	'Some judge1',	'2021-11-01 00:00:00',	'2021-11-12 00:00:00');
+insert into case_hearing_request (
+ auto_list_flag, hearing_type, required_duration_in_minutes, hearing_priority_type, number_of_physical_attendees, hearing_in_welsh_flag, private_hearing_required_flag, lead_judge_contract_type, first_date_time_of_hearing_must_be, hmcts_service_code, case_reference, hearing_request_received_date_time, external_case_reference, case_url_context_path, hmcts_internal_case_name, public_case_name, additional_security_required_flag, owning_location_id, case_restricted_flag, case_sla_start_date, hearing_request_version, hearing_id, interpreter_booking_required_flag,  listing_comments, requester, hearing_window_start_date_range, hearing_window_end_date_range)
+values ( 't'	,'hearingType1',	60,	'Priority type1',	4,'f','f','AAA2',null,'AAA2'	,9372710950276233,	'2021-08-10 11:20:00','EXT/REF123',	'https://www.google.com',	'Internal case name','Public case name',	't'	,'CMLC123',	't',	'2021-10-10 00:00:00',	1,	2100000005	,'t'	,	'Some listing comments1',	'Some judge1',	'2021-11-01 00:00:00',	'2021-11-12 00:00:00');
 
 
 insert into hearing_response(hearing_response_id, hearing_id, received_date_time, listing_status, listing_case_status, request_version, parties_notified_datetime)
@@ -37,6 +41,9 @@ insert into hearing_response(hearing_response_id, hearing_id, received_date_time
 values (8, '2100000003', '2019-08-10 11:00:00', 'listingStatus1-1', 'caselistingStatus1-1', 2, null);
 insert into hearing_response(hearing_response_id, hearing_id, received_date_time, listing_status, listing_case_status, request_version, parties_notified_datetime)
 values (9, '2100000003', '2019-08-10 11:00:00', 'listingStatus1-1', 'caselistingStatus1-1', 2, null);
+
+insert into hearing_response(hearing_response_id, hearing_id, received_date_time, listing_status, listing_case_status, request_version, parties_notified_datetime)
+values (10, '2100000005', '2024-08-10 11:00:00', 'listingStatus1-1', 'caselistingStatus1-1', 1, null);
 
 insert into hearing_day_details(hearing_day_id, hearing_response_id, start_date_time, end_date_time, venue_id, room_id)
 values (1, 1, '2021-01-10 11:00:00', '2020-11-10 11:00:00', 'venue1-1', 'room1-1');
@@ -63,3 +70,6 @@ insert into hearing_day_details(hearing_day_id, hearing_response_id, start_date_
 values (10, 8, '2023-06-15 09:00:00', '2023-06-15 10:00:00', 'venue2-1', 'room2-1');
 insert into hearing_day_details(hearing_day_id, hearing_response_id, start_date_time, end_date_time, venue_id, room_id)
 values (11, 9, '2023-06-15 12:00:00', '2023-06-16 13:00:00', 'venue2-1', 'room2-1');
+
+insert into hearing_day_details(hearing_day_id, hearing_response_id, start_date_time, end_date_time, venue_id, room_id)
+values (12, 10, null, '2023-06-15 10:00:00', 'venue2-1', 'room2-1');
--- src/main/java/uk/gov/hmcts/reform/hmc/repository/UnNotifiedHearingsRepositoryImpl.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/repository/UnNotifiedHearingsRepositoryImpl.java
@@ -25,26 +25,7 @@ public class UnNotifiedHearingsRepositoryImpl implements UnNotifiedHearingsRepos
     public List<Long> getUnNotifiedHearingsWithOutStartDateTo(String hmctsServiceCode,
                                                               LocalDateTime hearingStartDateFrom,
                                                               List<String> hearingStatus) {
-        StringBuffer hqlQuery = new StringBuffer("select hr.hearing.id FROM HearingResponseEntity hr "
-                                                     + "JOIN CaseHearingRequestEntity csr "
-                                                     + "ON hr.hearing.id = csr.hearing.id "
-                                                     + "join MaxHearingRequestVersionView mrv "
-                                                     + "ON csr.hearing.id = mrv.hearingId "
-                                                     + "JOIN HearingDayDetailsEntity hdd ON "
-                                                     + "hr.hearingResponseId = hdd.hearingResponse.hearingResponseId "
-                                                     + "JOIN HearingEntity he ON hr.hearing.id = he.id "
-                                                     + "where csr.hmctsServiceCode = :hmctsServiceCode "
-                                                     + "and (hr.requestVersion = mrv.maxHearingRequestVersion");
-        if (null != hearingStatus && hearingStatus.stream().anyMatch(e -> e.equalsIgnoreCase(CANCELLED))) {
-            hqlQuery.append(" OR (he.status = 'CANCELLED' AND he.id=hr.hearing.id)");
-        }
-        hqlQuery.append(") and hr.partiesNotifiedDateTime IS NULL "
-                            + "AND (hdd.startDateTime >= :hearingStartDateFrom ");
-        if (null != hearingStatus && hearingStatus.stream().anyMatch(e -> e.equalsIgnoreCase(CANCELLED))) {
-            hqlQuery.append("OR hdd.startDateTime IS NULL");
-        }
-        hqlQuery.append(") GROUP BY hr.hearing.id, hdd.startDateTime ");
-        hqlQuery.append("HAVING MIN(hdd.startDateTime) >= :hearingStartDateFrom ");
+        StringBuffer hqlQuery = getQueryForUnNotifiedHearings(hearingStatus);
         if (null != hearingStatus && hearingStatus.stream().anyMatch(e -> e.equalsIgnoreCase(CANCELLED))) {
             hqlQuery.append("OR hdd.startDateTime IS NULL");
         }
@@ -67,7 +48,28 @@ public class UnNotifiedHearingsRepositoryImpl implements UnNotifiedHearingsRepos
     public List<Long> getUnNotifiedHearingsWithStartDateTo(String hmctsServiceCode, LocalDateTime hearingStartDateFrom,
                                                            LocalDateTime hearingStartDateTo,
                                                            List<String> hearingStatus) {
-        StringBuffer hqlQuery = new StringBuffer("select hr.hearing.id FROM HearingResponseEntity hr "
+        StringBuffer hqlQuery = getQueryForUnNotifiedHearings(hearingStatus);
+        hqlQuery.append("AND MAX(hdd.endDateTime) >= :hearingStartDateTo ");
+        if (null != hearingStatus && hearingStatus.stream().anyMatch(e -> e.equalsIgnoreCase(CANCELLED))) {
+            hqlQuery.append("OR hdd.startDateTime IS NULL");
+        }
+        Query query = em.createQuery(hqlQuery.toString());
+        List hearings = query
+            .setParameter("hmctsServiceCode", hmctsServiceCode)
+            .setParameter("hearingStartDateFrom", hearingStartDateFrom)
+            .setParameter("hearingStartDateTo", hearingStartDateTo)
+            .setMaxResults(UN_NOTIFIED_HEARINGS_LIMIT)
+            .getResultList();
+        List<Long> hearingsLong = new ArrayList<>();
+        Iterator it = hearings.iterator();
+        while (it.hasNext()) {
+            hearingsLong.add((Long) it.next());
+        }
+        return hearingsLong;
+    }
+
+    private StringBuffer getQueryForUnNotifiedHearings(List<String> hearingStatus) {
+        StringBuffer hqlQuery = new StringBuffer("select distinct hr.hearing.id FROM HearingResponseEntity hr "
                                                      + "JOIN CaseHearingRequestEntity csr "
                                                      + "ON hr.hearing.id = csr.hearing.id "
                                                      + "join MaxHearingRequestVersionView mrv "
@@ -87,22 +89,6 @@ public class UnNotifiedHearingsRepositoryImpl implements UnNotifiedHearingsRepos
         }
         hqlQuery.append(") GROUP BY hr.hearing.id, hdd.startDateTime ");
         hqlQuery.append("HAVING MIN(hdd.startDateTime) >= :hearingStartDateFrom ");
-        hqlQuery.append("AND MAX(hdd.endDateTime) >= :hearingStartDateTo ");
-        if (null != hearingStatus && hearingStatus.stream().anyMatch(e -> e.equalsIgnoreCase(CANCELLED))) {
-            hqlQuery.append("OR hdd.startDateTime IS NULL");
-        }
-        Query query = em.createQuery(hqlQuery.toString());
-        List hearings = query
-            .setParameter("hmctsServiceCode", hmctsServiceCode)
-            .setParameter("hearingStartDateFrom", hearingStartDateFrom)
-            .setParameter("hearingStartDateTo", hearingStartDateTo)
-            .setMaxResults(UN_NOTIFIED_HEARINGS_LIMIT)
-            .getResultList();
-        List<Long> hearingsLong = new ArrayList<>();
-        Iterator it = hearings.iterator();
-        while (it.hasNext()) {
-            hearingsLong.add((Long) it.next());
-        }
-        return hearingsLong;
+        return hqlQuery;
     }
 }
