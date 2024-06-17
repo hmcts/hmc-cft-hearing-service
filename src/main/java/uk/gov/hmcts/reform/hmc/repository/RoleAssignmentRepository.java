ab92700bf160f9bd042207f88285aa48b98a7f36
Antony Leons <antony@leons.dev>
2024-06-17 12:29:20 +0100

    cleanup message processor

--- src/main/java/uk/gov/hmcts/reform/hmc/repository/RoleAssignmentRepository.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.hmc.repository;
-
-import uk.gov.hmcts.reform.hmc.data.RoleAssignmentResponse;
-
-public interface RoleAssignmentRepository {
-
-    RoleAssignmentResponse getRoleAssignments(String userId);
-
-}
