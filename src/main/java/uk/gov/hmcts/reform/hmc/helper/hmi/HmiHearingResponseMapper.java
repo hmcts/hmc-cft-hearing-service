c8a05403b9486e2818682dfc38a55575e0b37be9
Mark Dathorne <90186033+markdathornehmcts@users.noreply.github.com>
2024-07-09 13:30:54 +0100

    Update src/main/java/uk/gov/hmcts/reform/hmc/helper/hmi/HmiHearingResponseMapper.java
    Co-authored-by: RebeccaBaker <38425793+RebeccaBaker@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/hmc/helper/hmi/HmiHearingResponseMapper.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/helper/hmi/HmiHearingResponseMapper.java
@@ -307,6 +307,12 @@ public class HmiHearingResponseMapper {
             hearingResponseEntity.setListingStatus(hearingResponse.getHearing().getHearingStatus().getCode());
             if (hearingResponseEntity.getListingStatus().equals(ListAssistCaseStatus.CASE_CLOSED.name())) {
                 hearingResponseEntity.setRequestVersion(hearing.getLatestCaseHearingRequest().getVersionNumber());
+             if (hearingResponseEntity.getListingStatus().equals(ListAssistCaseStatus.CASE_CLOSED.name())) {
+                hearingResponseEntity.setRequestVersion(hearing.getLatestCaseHearingRequest().getVersionNumber());
+            } 
+            else 
+            {
+              hearingResponseEntity.setRequestVersion(hearingResponse.getHearing().getHearingCaseVersionId());
             }
         }
 
