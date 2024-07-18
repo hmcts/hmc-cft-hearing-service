8c964db5952539145000bfb895f8f8e91d73fc3e
Antony Leons <antony@leons.dev>
2024-07-18 15:12:45 +0100

    fix styles

--- src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/CftHearingServiceTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/CftHearingServiceTestAutomationAdapter.java
@@ -4,7 +4,6 @@ import uk.gov.hmcts.befta.BeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultBeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
-import uk.gov.hmcts.befta.util.BeftaUtils;
 
 public class CftHearingServiceTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
