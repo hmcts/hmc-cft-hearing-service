cafceed147de79b698a4ac00b962224d6db05d09
Antony Leons <antony@leons.dev>
2024-09-25 09:05:26 +0100

    Update V20240503_HMAN-581-AlterPendingRequests.sql

--- src/main/resources/db/migration/V20240503_HMAN-581-AlterPendingRequests.sql
+++ src/main/resources/db/migration/V20240503_HMAN-581-AlterPendingRequests.sql
@@ -1,4 +1,8 @@
-ALTER TABLE
-    public.pending_requests
-ADD
-    COLUMN IF NOT EXISTS message text;
+ALTER TABLE public.pending_requests
+    ALTER COLUMN hearing_id DROP NOT NULL,
+    ALTER COLUMN version_number DROP NOT NULL,
+    ALTER COLUMN submitted_date_time DROP NOT NULL,
+    ALTER COLUMN retry_count DROP NOT NULL,
+    ALTER COLUMN last_tried_date_time DROP NOT NULL,
+    ALTER COLUMN status DROP NOT NULL,
+    ADD COLUMN IF NOT EXISTS message TEXT;
