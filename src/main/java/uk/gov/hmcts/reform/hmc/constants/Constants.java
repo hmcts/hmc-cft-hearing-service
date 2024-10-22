78ff8e3019501d085e1a4bac2bafae1f3ae42eda
padmapriyanalam <67232857+padmapriyanalam@users.noreply.github.com>
2024-10-22 15:48:39 +0100

    HMAN-873-Azure connectivity issues (#527)
    * HMAN-873-Azure connectivity issues
    
    * HMAN-873-Azure connectivity issues
    
    * HMAN-873-Azure connectivity issues
    
    * HMAN-873-Add Idle time out for Azure session
    
    * HMAN-873-Revert the changes
    
    * HMAN-873- set idle time out to 30secs
    
    * HMAN-873-Add configuration builder to service bus
    
    * HMAN-873-Add configuration builder to service bus
    
    * HMAN-873-Add configuration builder to service bus
    
    * HMAN-873-Add configuration builder to service bus

--- src/main/java/uk/gov/hmcts/reform/hmc/constants/Constants.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/constants/Constants.java
@@ -76,4 +76,6 @@ public final class Constants {
     public static final String SERVICE_CODE_ABA1 = "ABA1";
     public static final String EXCEPTION_MESSAGE = "Hearing id: {} with Case reference: {} , Service Code: {}"
         + " and Error Description: {} updated to status {}";
+    public static final String AMQP_CACHE = "com.azure.core.amqp.cache";
+    public static final String AMQP_CACHE_VALUE = "true";
 }
