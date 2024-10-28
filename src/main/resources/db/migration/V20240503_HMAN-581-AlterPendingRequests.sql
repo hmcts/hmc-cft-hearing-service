5e4337ae4bd6561824061d4a1d65b070921f12b8
markdathorne <mark.dathorne@cgi.com>
2024-10-28 11:58:23 +0000

    HMAN-581 code tidy - remove redundant pending request table creation/alteration scripts

--- src/main/resources/db/migration/V20240503_HMAN-581-AlterPendingRequests.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE public.pending_requests
-    ALTER COLUMN hearing_id DROP NOT NULL,
-    ALTER COLUMN version_number DROP NOT NULL,
-    ALTER COLUMN submitted_date_time DROP NOT NULL,
-    ALTER COLUMN retry_count DROP NOT NULL,
-    ALTER COLUMN last_tried_date_time DROP NOT NULL,
-    ALTER COLUMN status DROP NOT NULL,
-    ADD COLUMN IF NOT EXISTS message TEXT;
