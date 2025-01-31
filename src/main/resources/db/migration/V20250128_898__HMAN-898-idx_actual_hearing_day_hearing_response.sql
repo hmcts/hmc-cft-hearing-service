4891042a32be6aec1fd7e6f74b0ae56e684ccbdf
padmapriyanalam <padmapriya.nalam@gmail.com>
2025-01-31 09:25:54 +0000

    HMAN-898 Add unique constraint

--- src/main/resources/db/migration/V20250128_898__HMAN-898-idx_actual_hearing_day_hearing_response.sql
+++ src/main/resources/db/migration/V20250128_898__HMAN-898-idx_actual_hearing_day_hearing_response.sql
@@ -1,3 +1,3 @@
 ALTER TABLE ONLY public.actual_hearing
-    ADD CONSTRAINT uc_actual_hearing_id_hearing_response_id UNIQUE (actual_hearing_id,hearing_response_id);
+    ADD CONSTRAINT uc_actual_hearing_id_hearing_response_id UNIQUE (hearing_response_id);
 
