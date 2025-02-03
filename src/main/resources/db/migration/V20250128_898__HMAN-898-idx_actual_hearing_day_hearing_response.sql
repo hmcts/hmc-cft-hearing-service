f5b996493f0adf531ae3d0ab08d1101c0a75627f
padmapriyanalam <padmapriya.nalam@gmail.com>
2025-02-03 15:06:03 +0000

    HMAN-898 Add unique DEFERRABLE constraint

--- src/main/resources/db/migration/V20250128_898__HMAN-898-idx_actual_hearing_day_hearing_response.sql
+++ src/main/resources/db/migration/V20250128_898__HMAN-898-idx_actual_hearing_day_hearing_response.sql
@@ -1,3 +1,3 @@
-ALTER TABLE ONLY public.actual_hearing
-    ADD CONSTRAINT uc_actual_hearing_id_hearing_response_id UNIQUE (hearing_response_id);
-
+ALTER TABLE public.actual_hearing
+ADD CONSTRAINT uc_actual_hearing_id_hearing_response_id UNIQUE (hearing_response_id)
+DEFERRABLE INITIALLY DEFERRED;
