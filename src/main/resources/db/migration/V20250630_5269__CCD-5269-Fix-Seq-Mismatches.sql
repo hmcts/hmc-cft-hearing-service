85a9bf42f79553bba1ccc93384919d60729c579e
Ben Lang <132359359+lang-ben@users.noreply.github.com>
2025-07-02 13:41:51 +0100

    Fix sequence reset SQL

--- src/main/resources/db/migration/V20250630_5269__CCD-5269-Fix-Seq-Mismatches.sql
+++ src/main/resources/db/migration/V20250630_5269__CCD-5269-Fix-Seq-Mismatches.sql
@@ -1,15 +1,13 @@
 -- Fix db sequence mismatches
 
 ---- Fix public.change_reasons_id_seq sequence
-ALTER SEQUENCE public.change_reasons_id_seq RESTART WITH 
-    (SELECT COALESCE(MAX(id), 0) + 1 FROM public.change_reasons);
+SELECT setval('public."change_reasons_id_seq"', (SELECT COALESCE(MAX(id), 0) + 1 FROM public.change_reasons));
 
 ALTER TABLE ONLY public.change_reasons
 ALTER COLUMN id SET DEFAULT nextval('public.change_reasons_id_seq');
 
 -- Fix public.reasonable_adjustments.request_id sequence
-ALTER SEQUENCE public.reasonable_adjustments_id_seq RESTART WITH 
-    (SELECT COALESCE(MAX(id), 0) + 1 FROM public.reasonable_adjustments);
+SELECT setval('public."reasonable_adjustments_id_seq"', (SELECT COALESCE(MAX(id), 0) + 1 FROM public.reasonable_adjustments));
 
 ALTER TABLE ONLY public.reasonable_adjustments
 ALTER COLUMN id SET DEFAULT nextval('public.reasonable_adjustments_id_seq');
