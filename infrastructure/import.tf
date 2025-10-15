8470022375496dfaebaf463014cdb285c96a2373
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-10-15 10:45:41 +0100

    Typo fix

--- infrastructure/import.tf
+++ infrastructure/import.tf
@@ -1,5 +1,5 @@
 removed {
-  from = module.postgresql_v15_replica[0].azurerm_postgresql_flexible_server_active_directory_administrator.pgsql_adadmin
+  from = module.postgresql_v15_replica.azurerm_postgresql_flexible_server_active_directory_administrator.pgsql_adadmin
 
   lifecycle {
     destroy = false
