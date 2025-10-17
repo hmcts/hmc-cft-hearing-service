9d230299dbab893a163b7c07a03eac1c720cf347
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-10-17 14:33:49 +0100

    Removed redundant import.tf

--- infrastructure/import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = module.postgresql_v15_replica[0].azurerm_postgresql_flexible_server_active_directory_administrator.pgsql_adadmin
-  id = "/subscriptions/8999dec3-0104-4a27-94ee-6588559729d1/resourceGroups/hmc-cft-hearing-service-postgres-db-v15-data-prod/providers/Microsoft.DBforPostgreSQL/flexibleServers/hmc-cft-hearing-service-postgres-db-v15-replica-prod/administrators/ef17f368-0aa5-43d5-9264-28080f83755e"
-}
