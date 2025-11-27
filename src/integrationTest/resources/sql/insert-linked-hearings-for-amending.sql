80c8ca196e3284fd54a05a5f5813d53f22ec4eaf
ridoutp <paul.ridout@cgi.com>
2025-11-27 11:13:51 +0000

    HMAN-993: Add case hearing requests to all hearings in test data
    Updated test data SQL scripts to include case hearing requests for all hearings.  This is to prevent "Cannot find latest case hearing request" errors if the hearing used to generate the linked hearing audit records happens to be one without a case hearing request.  Originally case hearing requests were only created for the first hearing in the test data as a shortcut.

--- src/integrationTest/resources/sql/insert-linked-hearings-for-amending.sql
+++ src/integrationTest/resources/sql/insert-linked-hearings-for-amending.sql
@@ -40,6 +40,23 @@ VALUES
  null, null, 'Some listing comments', '2025-10-01 12:00:00', 'user-added-comments',
  null);
 
+INSERT INTO case_hearing_request
+(case_hearing_id, auto_list_flag, hearing_type, required_duration_in_minutes, hearing_priority_type,
+ number_of_physical_attendees, hearing_in_welsh_flag, private_hearing_required_flag, lead_judge_contract_type, first_date_time_of_hearing_must_be,
+ hmcts_service_code, case_reference, hearing_request_received_date_time, external_case_reference, case_url_context_path,
+ hmcts_internal_case_name, public_case_name, additional_security_required_flag, owning_location_id, case_restricted_flag,
+ case_sla_start_date, hearing_request_version, hearing_id, interpreter_booking_required_flag, requester,
+ hearing_window_start_date_range, hearing_window_end_date_range, listing_comments, created_date_time, listing_auto_change_reason_code,
+ is_a_panel_flag)
+VALUES
+(2, false, 'Some hearing type', 5, 'Standard',
+ 0, false, false, '', null,
+ 'ABA1', '1111222233334444', now(), 'EXT/REF123', 'https://www.google.com',
+ 'Internal case name', 'Public case name', false, 'CMLC123', false,
+ current_date, 1, 2000000001, false, '',
+ null, null, 'Some listing comments', '2025-10-01 13:00:00', 'user-added-comments',
+ null);
+
 INSERT INTO hearing
 (hearing_id, status, linked_group_id, linked_order, is_linked_flag,
  error_code, error_description, created_date_time, updated_date_time, deployment_id,
@@ -67,10 +84,27 @@ INSERT INTO case_hearing_request
  hearing_window_start_date_range, hearing_window_end_date_range, listing_comments, created_date_time, listing_auto_change_reason_code,
  is_a_panel_flag)
 VALUES
-(2, false, 'Some hearing type', 5, 'Standard',
+(3, false, 'Some hearing type', 5, 'Standard',
  0, false, false, '', null,
- 'ABA1', '1111222233334444', now(), 'EXT/REF123', 'https://www.google.com',
+ 'ABA1', '2222333344445555', now(), 'EXT/REF123', 'https://www.google.com',
  'Internal case name', 'Public case name', false, 'CMLC123', false,
  current_date, 1, 2000000002, false, '',
- null, null, 'Some listing comments', '2025-10-01 13:00:00', 'user-added-comments',
+ null, null, 'Some listing comments', '2025-10-01 14:00:00', 'user-added-comments',
+ null);
+
+INSERT INTO case_hearing_request
+(case_hearing_id, auto_list_flag, hearing_type, required_duration_in_minutes, hearing_priority_type,
+ number_of_physical_attendees, hearing_in_welsh_flag, private_hearing_required_flag, lead_judge_contract_type, first_date_time_of_hearing_must_be,
+ hmcts_service_code, case_reference, hearing_request_received_date_time, external_case_reference, case_url_context_path,
+ hmcts_internal_case_name, public_case_name, additional_security_required_flag, owning_location_id, case_restricted_flag,
+ case_sla_start_date, hearing_request_version, hearing_id, interpreter_booking_required_flag, requester,
+ hearing_window_start_date_range, hearing_window_end_date_range, listing_comments, created_date_time, listing_auto_change_reason_code,
+ is_a_panel_flag)
+VALUES
+(4, false, 'Some hearing type', 5, 'Standard',
+ 0, false, false, '', null,
+ 'ABA1', '2222333344445555', now(), 'EXT/REF123', 'https://www.google.com',
+ 'Internal case name', 'Public case name', false, 'CMLC123', false,
+ current_date, 1, 2000000003, false, '',
+ null, null, 'Some listing comments', '2025-10-01 15:00:00', 'user-added-comments',
  null);
