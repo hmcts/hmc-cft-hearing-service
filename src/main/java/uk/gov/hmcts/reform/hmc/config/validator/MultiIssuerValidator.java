e343279c54499f093aff371c6f40fb0705f3a340
ridoutp <paul.ridout@cgi.com>
2026-04-13 10:35:44 +0100

    HMAN-1084: Validate JWT issuer
    Changed OIDC_ISSUER_FORGEROCK environment variable name back to OIDC_ISSUER.  The rse-cft-lib code sets the value of the OIDC_ISSUER environment variable when authMode is local.  Renaming the variable did not stop issuer validation from working when authMode is local as rse-cft-lib overrides the JWTDecoder, but decided to keep the name the same out of caution.
    
    Added a debug statement to MultiIssuerValidator constructor to output list of issuers it is initialised with.

--- src/main/java/uk/gov/hmcts/reform/hmc/config/validator/MultiIssuerValidator.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/config/validator/MultiIssuerValidator.java
@@ -22,6 +22,7 @@ public class MultiIssuerValidator implements OAuth2TokenValidator<Jwt> {
     public MultiIssuerValidator(List<String> validIssuers) {
         Assert.notEmpty(validIssuers, "Valid issuers list should not be null or empty");
         Assert.noNullElements(validIssuers, "Valid issuers list should not contain any null elements");
+        log.debug("Valid issuers list: {}", validIssuers);
         this.validIssuers = validIssuers;
     }
 
