78c4e9caba92b10e9337af5578f7c1876cb36eb0
padmapriyanalam <padmapriya.nalam@gmail.com>
2026-05-12 14:23:51 +0100

    HMAN-1102 Add new table actual_hearing_audit

--- src/main/java/uk/gov/hmcts/reform/hmc/data/ActualHearingAuditEntity.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/data/ActualHearingAuditEntity.java
@@ -13,7 +13,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
