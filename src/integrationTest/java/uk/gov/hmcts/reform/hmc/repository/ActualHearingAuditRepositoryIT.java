9913e31cff441b54c054c2f786adc9d880453ab7
padmapriyanalam <padmapriya.nalam@gmail.com>
2026-05-13 13:09:36 +0100

    HMAN-1102 Add new table actual_hearing_audit

--- src/integrationTest/java/uk/gov/hmcts/reform/hmc/repository/ActualHearingAuditRepositoryIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/hmc/repository/ActualHearingAuditRepositoryIT.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.hmc.repository;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 import uk.gov.hmcts.reform.hmc.BaseTest;
 import uk.gov.hmcts.reform.hmc.data.ActualHearingAuditEntity;
@@ -12,8 +11,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ActualHearingAuditRepositoryIT extends BaseTest {
 
-    @Autowired
-    ActualHearingAuditRepository actualHearingAuditRepository;
+    private final ActualHearingAuditRepository actualHearingAuditRepository;
+
+    ActualHearingAuditRepositoryIT(ActualHearingAuditRepository actualHearingAuditRepository) {
+        this.actualHearingAuditRepository = actualHearingAuditRepository;
+    }
 
     private static final String INSERT_ACTUAL_HEARING_AUDIT_SCRIPT = "classpath:sql/insert-actual_hearing_audit.sql";
     private static final String DELETE_HEARING_DATA_SCRIPT = "classpath:sql/delete-hearing-tables.sql";
@@ -22,22 +24,24 @@ class ActualHearingAuditRepositoryIT extends BaseTest {
     @Sql(scripts = {DELETE_HEARING_DATA_SCRIPT, INSERT_ACTUAL_HEARING_AUDIT_SCRIPT})
     void findByHearingResponseId() {
         List<ActualHearingAuditEntity> auditEntityList = actualHearingAuditRepository.findByHearingResponseId(1L);
+        ActualHearingAuditEntity auditEntity = auditEntityList.getFirst();
         assertEquals(1, auditEntityList.size());
-        assertEquals(2000000000L, auditEntityList.get(0).getHearingId());
-        assertEquals(1L, auditEntityList.get(0).getHearingResponseId());
+        assertEquals(2000000000L, auditEntity.getHearingId());
+        assertEquals(1L, auditEntity.getHearingResponseId());
         assertEquals("Some audit details for hearing 2000000000",
-                     auditEntityList.get(0).getActualHearingAuditRecord().get("auditDetails").asText());
+                     auditEntity.getActualHearingAuditRecord().get("auditDetails").asText());
     }
 
     @Test
     @Sql(scripts = {DELETE_HEARING_DATA_SCRIPT, INSERT_ACTUAL_HEARING_AUDIT_SCRIPT})
     void findByHearingId() {
         List<ActualHearingAuditEntity> auditEntityList = actualHearingAuditRepository.findByHearingId(2000000001L);
+        ActualHearingAuditEntity auditEntity = auditEntityList.getFirst();
         assertEquals(1, auditEntityList.size());
-        assertEquals(2000000001L, auditEntityList.get(0).getHearingId());
-        assertEquals(2L, auditEntityList.get(0).getHearingResponseId());
+        assertEquals(2000000001L, auditEntity.getHearingId());
+        assertEquals(2L, auditEntity.getHearingResponseId());
         assertEquals("Some audit details for hearing 2000000001",
-                     auditEntityList.get(0).getActualHearingAuditRecord().get("auditDetails").asText());
+                     auditEntity.getActualHearingAuditRecord().get("auditDetails").asText());
     }
 
 }
