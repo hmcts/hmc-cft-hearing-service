234f485e7bf98aeddf3627fbe93e750fb2ebfb7a
padmapriyanalam <padmapriya.nalam@gmail.com>
2026-06-18 15:15:00 +0100

    HMAN-1100  PUT hearingactuals - hearingcompletion

--- src/main/java/uk/gov/hmcts/reform/hmc/service/HearingActualsServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/service/HearingActualsServiceImpl.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.hmc.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
