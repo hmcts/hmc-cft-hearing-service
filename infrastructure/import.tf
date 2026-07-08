7f191b25473550e4a9abbbaaa1f46d9d83819e89
dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>
2026-07-08 11:59:16 +0100

    Delete infrastructure/import.tf

--- infrastructure/import.tf
+++
@@ -1,4 +0,0 @@
-import {
- id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/hmc-cft-hearing-service-postgres-db-v15-data-aat/providers/Microsoft.DBforPostgreSQL/flexibleServers/hmc-cft-hearing-service-postgres-db-v15-replica-aat/administrators/14b22215-46e6-48a9-8681-e8cefe66236a"
- to = module.postgresql_v15.azurerm_postgresql_flexible_server_active_directory_administrator.pgsql_additional_principal_admin["c860eaa0-74be-4731-8370-db94c5fdad81"]
-}
