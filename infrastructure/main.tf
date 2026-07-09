ef2247c0706417acfb302fd070b1924375b24e06
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-07-09 08:51:30 +0100

    Revert postgres replica to master (#754)
    * revert replica to master
    
    * use jenkins ptl identity for replica
    
    * fix typo
    
    * fix typo in identity

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -97,15 +97,19 @@ resource "azurerm_key_vault_secret" "POSTGRES-DATABASE" {
 // DB version 15 Replication          //
 ////////////////////////////////////////
 
+data "azuread_service_principal" "jenkins_ptl" {
+  display_name = "jenkins-cftptl-intsvc-mi"
+}
+
 module "postgresql_v15_replica" {
-  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=DTSPO-30107-additional-postgres-admins"
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
   count  = var.enable_replica ? 1 : 0
   providers = {
     azurerm.postgres_network = azurerm.postgres_network
   }
 
   subnet_suffix        = "expanded"
-  admin_user_object_id = var.jenkins_AAD_objectId
+  admin_user_object_id = data.azuread_service_principal.jenkins_ptl.object_id
   business_area        = "cft"
   common_tags          = var.common_tags
   component            = var.component
