6e07c58bff94cc97d069ef4ed9d18be6e7653661
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-07-09 12:12:46 +0100

    ignore lifecycle changes to additional admin

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -131,9 +131,10 @@ module "postgresql_v15_replica" {
   source_server_id    = var.primary_server_id
   high_availability   = false
 
-}
+  lifecycle {
+    ignore_changes = [
+      module.postgresql_v15_replica[0].azurerm_postgresql_flexible_server_active_directory_administrator.pgsql_additional_principal_admin["14b22215-46e6-48a9-8681-e8cefe66236a"]
+    ]
+  }
 
-import {
-  to = module.postgresql_v15_replica[0].azurerm_postgresql_flexible_server_active_directory_administrator.pgsql_additional_principal_admin["14b22215-46e6-48a9-8681-e8cefe66236a"]
-  id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/hmc-cft-hearing-service-postgres-db-v15-data-aat/providers/Microsoft.DBforPostgreSQL/flexibleServers/hmc-cft-hearing-service-postgres-db-v15-replica-aat/administrators/14b22215-46e6-48a9-8681-e8cefe66236a"
 }
